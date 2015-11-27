9466afba71b9a48f1684a1534b78c2659f153833
Dominick Baier <dbaier@leastprivilege.com>
2015-11-27 09:11:56 +0100

    Moved option classes

--- src/Host/Startup.cs
+++ src/Host/Startup.cs
@@ -7,13 +7,10 @@ namespace IdentityServer4.Core
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
             app.UseIISPlatformHandler();
@@ -24,7 +21,6 @@ namespace IdentityServer4.Core
             });
         }
 
-        // Entry point for the application.
         public static void Main(string[] args) => WebApplication.Run<Startup>(args);
     }
 }
