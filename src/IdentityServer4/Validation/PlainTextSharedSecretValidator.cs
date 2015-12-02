8e6f1a64224d4d1dda972aaebe51cc3d4764a75b
Dominick Baier <dbaier@leastprivilege.com>
2015-12-02 20:28:58 +0100

    ported secret validator tests

---
+++ src/IdentityServer4/Validation/PlainTextSharedSecretValidator.cs
@@ -0,0 +1,98 @@
+﻿/*
+ * Copyright 2014, 2015 Dominick Baier, Brock Allen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using IdentityModel;
+using IdentityServer4.Core.Extensions;
+using IdentityServer4.Core.Models;
+using IdentityServer4.Core.Services;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace IdentityServer4.Core.Validation
+{
+    /// <summary>
+    /// Validates a secret stored in plain text
+    /// </summary>
+    public class PlainTextSharedSecretValidator : ISecretValidator
+    {
+        private readonly ILogger _logger;
+
+        public PlainTextSharedSecretValidator(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<PlainTextSharedSecretValidator>();
+        }
+
+        /// <summary>
+        /// Validates a secret
+        /// </summary>
+        /// <param name="secrets">The stored secrets.</param>
+        /// <param name="parsedSecret">The received secret.</param>
+        /// <returns>
+        /// A validation result
+        /// </returns>
+        /// <exception cref="System.ArgumentException">id or credential is missing.</exception>
+        public Task<SecretValidationResult> ValidateAsync(IEnumerable<Secret> secrets, ParsedSecret parsedSecret)
+        {
+            var fail = Task.FromResult(new SecretValidationResult { Success = false });
+            var success = Task.FromResult(new SecretValidationResult { Success = true });
+
+            if (parsedSecret.Type != Constants.ParsedSecretTypes.SharedSecret)
+            {
+                _logger.LogVerbose("Parsed secret should not be of type: {type}", parsedSecret.Type ?? "null");
+                return fail;
+            }
+
+            var sharedSecret = parsedSecret.Credential as string;
+
+            if (parsedSecret.Id.IsMissing() || sharedSecret.IsMissing())
+            {
+                throw new ArgumentException("Id or Credential is missing.");
+            }
+
+            foreach (var secret in secrets)
+            {
+                var secretDescription = string.IsNullOrEmpty(secret.Description) ? "no description" : secret.Description;
+
+                // this validator is only applicable to shared secrets
+                if (secret.Type != Constants.SecretTypes.SharedSecret)
+                {
+                    _logger.LogVerbose("Skipping secret: {description}, secret is not of type {type}.", secretDescription, Constants.SecretTypes.SharedSecret);
+                    continue;
+                }
+
+                // check if client secret is still valid
+                if (secret.Expiration.HasExpired())
+                {
+                    _logger.LogVerbose("Skipping secret: {description}, secret is expired.", secretDescription);
+                    continue;
+                }
+
+                // use time constant string comparison
+                var isValid = TimeConstantComparer.IsEqual(sharedSecret, secret.Value);
+
+                if (isValid)
+                {
+                    return success;
+                }
+            }
+
+            _logger.LogVerbose("No matching plain text secret found.");
+            return fail;
+        }
+    }
+}
---
+++ test/UnitTests/Validation/Secrets/PlainTextClientSecretValidation.cs
@@ -0,0 +1,178 @@
+﻿/*
+ * Copyright 2014, 2015 Dominick Baier, Brock Allen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using FluentAssertions;
+using IdentityServer4.Core;
+using IdentityServer4.Core.Models;
+using IdentityServer4.Core.Services;
+using IdentityServer4.Core.Services.InMemory;
+using IdentityServer4.Core.Validation;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace IdentityServer4.Tests.Validation.Secrets
+{
+    public class PlainTextClientSecretValidation
+    {
+        const string Category = "Secrets - PlainText Shared Secret Validation";
+
+        ISecretValidator _validator = new PlainTextSharedSecretValidator(new LoggerFactory());
+        IClientStore _clients = new InMemoryClientStore(ClientValidationTestClients.Get());
+
+        [Fact]
+        [Trait("Category", Category)]
+        public async Task Valid_Single_Secret()
+        {
+            var clientId = "single_secret_no_protection_no_expiration";
+            var client = await _clients.FindClientByIdAsync(clientId);
+
+            var secret = new ParsedSecret
+            {
+                Id = clientId,
+                Credential = "secret",
+                Type = Constants.ParsedSecretTypes.SharedSecret
+            };
+
+            var result = await _validator.ValidateAsync(client.ClientSecrets, secret);
+
+            result.Success.Should().BeTrue();
+        }
+
+        [Fact]
+        [Trait("Category", Category)]
+        public async Task Invalid_Credential_Type()
+        {
+            var clientId = "single_secret_no_protection_no_expiration";
+            var client = await _clients.FindClientByIdAsync(clientId);
+
+            var secret = new ParsedSecret
+            {
+                Id = clientId,
+                Credential = "secret",
+                Type = "invalid"
+            };
+
+            var result = await _validator.ValidateAsync(client.ClientSecrets, secret);
+
+            result.Success.Should().BeFalse();
+        }
+
+        [Fact]
+        [Trait("Category", Category)]
+        public async Task Valid_Multiple_Secrets_No_Protection()
+        {
+            var clientId = "multiple_secrets_no_protection";
+            var client = await _clients.FindClientByIdAsync(clientId);
+
+            var secret = new ParsedSecret
+            {
+                Id = clientId,
+                Credential = "secret",
+                Type = Constants.ParsedSecretTypes.SharedSecret
+            };
+
+            var result = await _validator.ValidateAsync(client.ClientSecrets, secret);
+            result.Success.Should().BeTrue();
+
+            secret.Credential = "foobar";
+            result = await _validator.ValidateAsync(client.ClientSecrets, secret);
+            result.Success.Should().BeTrue();
+
+            secret.Credential = "quux";
+            result = await _validator.ValidateAsync(client.ClientSecrets, secret);
+            result.Success.Should().BeTrue();
+
+            secret.Credential = "notexpired";
+            result = await _validator.ValidateAsync(client.ClientSecrets, secret);
+            result.Success.Should().BeTrue();
+        }
+
+        [Fact]
+        [Trait("Category", Category)]
+        public async Task Invalid_Single_Secret()
+        {
+            var clientId = "single_secret_no_protection_no_expiration";
+            var client = await _clients.FindClientByIdAsync(clientId);
+
+            var secret = new ParsedSecret
+            {
+                Id = clientId,
+                Credential = "invalid",
+                Type = Constants.ParsedSecretTypes.SharedSecret
+            };
+
+            var result = await _validator.ValidateAsync(client.ClientSecrets, secret);
+
+            result.Success.Should().BeFalse();
+        }
+
+        [Fact]
+        [Trait("Category", Category)]
+        public async Task Expired_Secret()
+        {
+            var clientId = "multiple_secrets_no_protection";
+            var client = await _clients.FindClientByIdAsync(clientId);
+
+            var secret = new ParsedSecret
+            {
+                Id = clientId,
+                Credential = "expired",
+                Type = Constants.ParsedSecretTypes.SharedSecret
+            };
+
+            var result = await _validator.ValidateAsync(client.ClientSecrets, secret);
+            result.Success.Should().BeFalse();
+        }
+
+        [Fact]
+        [Trait("Category", Category)]
+        public async Task Invalid_Multiple_Secrets()
+        {
+            var clientId = "multiple_secrets_no_protection";
+            var client = await _clients.FindClientByIdAsync(clientId);
+
+            var secret = new ParsedSecret
+            {
+                Id = clientId,
+                Credential = "invalid",
+                Type = Constants.ParsedSecretTypes.SharedSecret
+            };
+
+            var result = await _validator.ValidateAsync(client.ClientSecrets, secret);
+            result.Success.Should().BeFalse();
+        }
+
+        [Fact]
+        [Trait("Category", Category)]
+        public async Task Client_with_no_Secret_Should_Throw()
+        {
+            var clientId = "no_secret_client";
+            var client = await _clients.FindClientByIdAsync(clientId);
+
+            var secret = new ParsedSecret
+            {
+                Id = clientId,
+                Type = Constants.ParsedSecretTypes.SharedSecret
+            };
+
+            Func<Task> act = () => _validator.ValidateAsync(client.ClientSecrets, secret);
+
+            act.ShouldThrow<ArgumentException>();
+        }
+    }
+}
