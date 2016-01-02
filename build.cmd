b7b45ed453718d57a0ec3e1171ef69e6873e1109
Dominick Baier <dbaier@leastprivilege.com>
2016-01-02 15:03:20 +0100

    fixed build script

--- build.cmd
+++ build.cmd
@@ -1,18 +1,11 @@
 @echo off
 cd %~dp0
 
-SETLOCAL
-SET CACHED_NUGET=%LocalAppData%\NuGet\NuGet.exe
-
-IF EXIST %CACHED_NUGET% goto copynuget
-echo Downloading latest version of NuGet.exe...
-IF NOT EXIST %LocalAppData%\NuGet md %LocalAppData%\NuGet
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest 'https://www.nuget.org/nuget.exe' -OutFile '%CACHED_NUGET%'"
-
-:copynuget
 IF EXIST .nuget\nuget.exe goto restore
+
+echo Downloading nuget.exe
 md .nuget
-copy %CACHED_NUGET% .nuget\nuget.exe > nul
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest 'https://dist.nuget.org/win-x86-commandline/latest/nuget.exe' -OutFile '.nuget\nuget.exe'"
 
 :restore
 IF EXIST packages\psake goto run
