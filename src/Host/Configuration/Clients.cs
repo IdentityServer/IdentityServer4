1dc0cee0469191f1d0da8d1bf6989ee48aad3044
Brock Allen <brockallen@gmail.com>
2016-01-08 10:37:43 -0500

    add config for js oidc sample

--- src/Host/Configuration/Clients.cs
+++ src/Host/Configuration/Clients.cs
@@ -141,7 +141,42 @@ namespace Host.Configuration
                     {
                         "api1", "api2"
                     }
-                }
+                },
+                
+                ///////////////////////////////////////////
+                // JS OIDC Sample
+                //////////////////////////////////////////
+                new Client
+                {
+                    ClientId = "js_oidc",
+                    ClientName = "JavaScript OIDC Client",
+                    ClientUri = "http://identityserver.io",
+
+                    Flow = Flows.Implicit,
+                    RedirectUris = new List<string>
+                    {
+                        "http://localhost:7017/index.html",
+                        "http://localhost:7017/silent_renew.html",
+                    },
+                    PostLogoutRedirectUris = new List<string>
+                    {
+                        "http://localhost:7017/index.html",
+                    },
+
+                    AllowedCorsOrigins = new List<string>
+                    {
+                        "http://localhost:7017"
+                    },
+
+                    AllowedScopes = new List<string>
+                    {
+                        StandardScopes.OpenId.Name,
+                        StandardScopes.Profile.Name,
+                        StandardScopes.Email.Name,
+                        StandardScopes.Roles.Name,
+                        "api1", "api2"
+                    }
+                },
             };
         }
     }
