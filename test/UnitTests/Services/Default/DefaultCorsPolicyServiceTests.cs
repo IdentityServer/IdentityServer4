0614db687442c1f6adcd3fff0ef1a565ad9cc017
Brock Allen <brockallen@gmail.com>
2016-01-08 11:59:24 -0500

    tests

---
+++ test/UnitTests/Services/Default/DefaultCorsPolicyServiceTests.cs
@@ -0,0 +1,64 @@
+﻿
+using FluentAssertions;
+using IdentityServer4.Core.Services.Default;
+using UnitTests.Common;
+using Xunit;
+
+namespace UnitTests.Services.Default
+{
+    public class DefaultCorsPolicyServiceTests
+    {
+        const string Category = "DefaultCorsPolicyService";
+
+        DefaultCorsPolicyService subject;
+
+        public DefaultCorsPolicyServiceTests()
+        {
+            subject = new DefaultCorsPolicyService(new FakeLogger<DefaultCorsPolicyService>());
+        }
+
+        [Fact]
+        [Trait("Category", Category)]
+        public void IsOriginAllowed_OriginIsAllowed_ReturnsTrue()
+        {
+            subject.AllowedOrigins.Add("http://foo");
+            subject.IsOriginAllowedAsync("http://foo").Result.Should().Be(true);
+        }
+
+        [Fact]
+        [Trait("Category", Category)]
+        public void IsOriginAllowed_OriginIsNotAllowed_ReturnsFalse()
+        {
+            subject.AllowedOrigins.Add("http://foo");
+            subject.IsOriginAllowedAsync("http://bar").Result.Should().Be(false);
+        }
+
+        [Fact]
+        [Trait("Category", Category)]
+        public void IsOriginAllowed_OriginIsInAllowedList_ReturnsTrue()
+        {
+            subject.AllowedOrigins.Add("http://foo");
+            subject.AllowedOrigins.Add("http://bar");
+            subject.AllowedOrigins.Add("http://baz");
+            subject.IsOriginAllowedAsync("http://bar").Result.Should().Be(true);
+        }
+
+        [Fact]
+        [Trait("Category", Category)]
+        public void IsOriginAllowed_OriginIsNotInAllowedList_ReturnsFalse()
+        {
+            subject.AllowedOrigins.Add("http://foo");
+            subject.AllowedOrigins.Add("http://bar");
+            subject.AllowedOrigins.Add("http://baz");
+            subject.IsOriginAllowedAsync("http://quux").Result.Should().Be(false);
+        }
+
+        [Fact]
+        [Trait("Category", Category)]
+        public void IsOriginAllowed_AllowAllTrue_ReturnsTrue()
+        {
+            subject.AllowAll = true;
+            subject.IsOriginAllowedAsync("http://foo").Result.Should().Be(true);
+        }
+    }
+}
