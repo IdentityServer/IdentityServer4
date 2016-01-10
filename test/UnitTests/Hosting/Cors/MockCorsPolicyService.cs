e1057295ef41e90b43b95acf2bc90ab821402288
Brock Allen <brockallen@gmail.com>
2016-01-10 12:45:49 +0000

    tests for policy provider

---
+++ test/UnitTests/Hosting/Cors/MockCorsPolicyService.cs
@@ -0,0 +1,20 @@
+﻿using IdentityServer4.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UnitTests.Hosting.Cors
+{
+    public class MockCorsPolicyService : ICorsPolicyService
+    {
+        public bool WasCalled { get; set; }
+        public bool Response { get; set; }
+
+        public Task<bool> IsOriginAllowedAsync(string origin)
+        {
+            WasCalled = true;
+            return Task.FromResult(Response);
+        }
+    }
+}
---
+++ test/UnitTests/Hosting/Cors/PolicyProviderTests.cs
@@ -0,0 +1,133 @@
+﻿using FluentAssertions;
+using IdentityServer4.Core.Hosting.Cors;
+using Microsoft.AspNet.Http.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UnitTests.Common;
+using Xunit;
+
+namespace UnitTests.Hosting.Cors
+{
+    public class PolicyProviderTests
+    {
+        const string Category = "PolicyProvider";
+
+        PolicyProvider _subject;
+        List<string> _allowedPaths = new List<string>();
+
+        MockCorsPolicyService _mockPolicy = new MockCorsPolicyService();
+
+        public PolicyProviderTests()
+        {
+            Init();
+        }
+
+        public void Init()
+        {
+            _subject = new PolicyProvider(
+                new FakeLogger<PolicyProvider>(),
+                _allowedPaths,
+                _mockPolicy);
+        }
+
+        [Theory]
+        [InlineData("/foo")]
+        [InlineData("/foo/")]
+        [InlineData("/bar")]
+        [InlineData("/bar/")]
+        [InlineData("/baz/quux")]
+        [InlineData("/baz/quux/")]
+        [Trait("Category", Category)]
+        public async Task valid_paths_should_call_policy_service(string path)
+        {
+            _allowedPaths.AddRange(new string[] {
+                "/foo",
+                "/bar/",
+                "/baz/quux",
+                "/baz/quux/",
+            });
+            Init();
+
+            var ctx = new DefaultHttpContext();
+            ctx.Request.Scheme = "https";
+            ctx.Request.Host = new Microsoft.AspNet.Http.HostString("server");
+            ctx.Request.Path = new Microsoft.AspNet.Http.PathString(path);
+            ctx.Request.Headers.Add("Origin", "http://notserver");
+
+            var response = await _subject.GetPolicyAsync(ctx, null);
+
+            _mockPolicy.WasCalled.Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData("/xoxo")]
+        [InlineData("/xoxo/")]
+        [InlineData("/foo/xoxo")]
+        [InlineData("/baz/quux/xoxo")]
+        [Trait("Category", Category)]
+        public async Task invalid_paths_should_not_call_policy_service(string path)
+        {
+            _allowedPaths.AddRange(new string[] {
+                "/foo",
+                "/bar",
+                "/baz/quux"
+            });
+            Init();
+
+            var ctx = new DefaultHttpContext();
+            ctx.Request.Scheme = "https";
+            ctx.Request.Host = new Microsoft.AspNet.Http.HostString("server");
+            ctx.Request.Path = new Microsoft.AspNet.Http.PathString(path);
+            ctx.Request.Headers.Add("Origin", "http://notserver");
+
+            var response = await _subject.GetPolicyAsync(ctx, null);
+
+            _mockPolicy.WasCalled.Should().BeFalse();
+        }
+
+        [Fact]
+        [Trait("Category", Category)]
+        public async Task origin_same_as_server_should_not_call_policy()
+        {
+            _allowedPaths.AddRange(new string[] {
+                "/foo",
+            });
+            Init();
+
+            var ctx = new DefaultHttpContext();
+            ctx.Request.Scheme = "https";
+            ctx.Request.Host = new Microsoft.AspNet.Http.HostString("server");
+            ctx.Request.Path = new Microsoft.AspNet.Http.PathString("/foo");
+            ctx.Request.Headers.Add("Origin", "https://server");
+
+            var response = await _subject.GetPolicyAsync(ctx, null);
+
+            _mockPolicy.WasCalled.Should().BeFalse();
+        }
+
+        [Theory]
+        [InlineData("https://notserver")]
+        [InlineData("http://server")]
+        [Trait("Category", Category)]
+        public async Task origin_not_same_as_server_should_call_policy(string origin)
+        {
+            _allowedPaths.AddRange(new string[] {
+                "/foo",
+            });
+            Init();
+
+            var ctx = new DefaultHttpContext();
+            ctx.Request.Scheme = "https";
+            ctx.Request.Host = new Microsoft.AspNet.Http.HostString("server");
+            ctx.Request.Path = new Microsoft.AspNet.Http.PathString("/foo");
+            ctx.Request.Headers.Add("Origin", origin);
+
+            var response = await _subject.GetPolicyAsync(ctx, null);
+
+            _mockPolicy.WasCalled.Should().BeTrue();
+        }
+
+    }
+}
