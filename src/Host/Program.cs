2c670d37a81bacb33f7e86768e805840419e28ec
Dominick Baier <dbaier@leastprivilege.com>
2016-06-13 17:15:17 +0200

    changed kestrel to use same port as IIS

--- src/Host/Program.cs
+++ src/Host/Program.cs
@@ -9,6 +9,7 @@ namespace Host
         {
             var host = new WebHostBuilder()
                 .UseKestrel()
+                .UseUrls("http://localhost:1941")
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
