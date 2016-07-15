9fd2a54936431600da93ddc98d6bc62a56ca1a43
Dominick Baier <dbaier@leastprivilege.com>
2016-07-15 06:46:38 +0200

    made category constant for authZ tests

--- test/IdentityServer.UnitTests/Validation/AuthorizeRequest Validation/Authorize_ClientValidation_Valid.cs
+++ test/IdentityServer.UnitTests/Validation/AuthorizeRequest Validation/Authorize_ClientValidation_Valid.cs
@@ -14,10 +14,13 @@ namespace IdentityServer4.Tests.Validation.AuthorizeRequest
 
     public class Authorize_ClientValidation_Valid
     {
+        const string Category = "AuthorizeRequest Client Validation - Valid";
+
         IdentityServerOptions _options = TestIdentityServerOptions.Create();
 
+
         [Fact]
-        [Trait("Category", "AuthorizeRequest Client Validation - Valid")]
+        [Trait("Category", Category)]
         public async Task Valid_OpenId_Code_Request()
         {
             var parameters = new NameValueCollection();
@@ -33,7 +36,7 @@ namespace IdentityServer4.Tests.Validation.AuthorizeRequest
         }
 
         [Fact]
-        [Trait("Category", "AuthorizeRequest Client Validation - Valid")]
+        [Trait("Category", Category)]
         public async Task Valid_Resource_Code_Request()
         {
             var parameters = new NameValueCollection();
@@ -49,7 +52,7 @@ namespace IdentityServer4.Tests.Validation.AuthorizeRequest
         }
 
         [Fact]
-        [Trait("Category", "AuthorizeRequest Client Validation - Valid")]
+        [Trait("Category", Category)]
         public async Task Valid_Mixed_Code_Request()
         {
             var parameters = new NameValueCollection();
@@ -65,7 +68,7 @@ namespace IdentityServer4.Tests.Validation.AuthorizeRequest
         }
 
         [Fact]
-        [Trait("Category", "AuthorizeRequest Client Validation - Valid")]
+        [Trait("Category", Category)]
         public async Task Valid_Mixed_Code_Request_Multiple_Scopes()
         {
             var parameters = new NameValueCollection();
@@ -81,7 +84,7 @@ namespace IdentityServer4.Tests.Validation.AuthorizeRequest
         }
 
         [Fact]
-        [Trait("Category", "AuthorizeRequest Client Validation - Valid")]
+        [Trait("Category", Category)]
         public async Task Valid_OpenId_IdTokenToken_Request()
         {
             var parameters = new NameValueCollection();
@@ -98,7 +101,7 @@ namespace IdentityServer4.Tests.Validation.AuthorizeRequest
         }
 
         [Fact]
-        [Trait("Category", "AuthorizeRequest Client Validation - Valid")]
+        [Trait("Category", Category)]
         public async Task Valid_Mixed_IdTokenToken_Request()
         {
             var parameters = new NameValueCollection();
@@ -115,7 +118,7 @@ namespace IdentityServer4.Tests.Validation.AuthorizeRequest
         }
 
         [Fact]
-        [Trait("Category", "AuthorizeRequest Client Validation - Valid")]
+        [Trait("Category", Category)]
         public async Task Valid_Mixed_IdTokenToken_Request_Multiple_Scopes()
         {
             var parameters = new NameValueCollection();
@@ -132,7 +135,7 @@ namespace IdentityServer4.Tests.Validation.AuthorizeRequest
         }
 
         [Fact]
-        [Trait("Category", "AuthorizeRequest Client Validation - Valid")]
+        [Trait("Category", Category)]
         public async Task Valid_Resource_Token_Request()
         {
             var parameters = new NameValueCollection();
