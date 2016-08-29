6cb32641fd36ea77e03035545fd2a32b42d7d004
Brock Allen <brockallen@gmail.com>
2016-08-29 08:55:12 -0400

    unneeded namespaces

--- src/IdentityServer4/Hosting/AuthenticationMiddleware.cs
+++ src/IdentityServer4/Hosting/AuthenticationMiddleware.cs
@@ -1,12 +1,8 @@
 ﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
-using IdentityModel;
-using IdentityServer4.Configuration;
 using Microsoft.AspNetCore.Http;
-using System.Linq;
 using System.Threading.Tasks;
-using System;
 using IdentityServer4.Hosting.Cookies;
 
 namespace IdentityServer4.Hosting
