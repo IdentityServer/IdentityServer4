d06498051475096f95711569af2353f324f46470
Dominick Baier <dbaier@leastprivilege.com>
2016-08-30 18:30:16 +0200

    add copyright

--- src/IdentityServer4/Extensions/DictionaryExtensions.cs
+++ src/IdentityServer4/Extensions/DictionaryExtensions.cs
@@ -1,4 +1,7 @@
-﻿using Newtonsoft.Json.Linq;
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Reflection;
