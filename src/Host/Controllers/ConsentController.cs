eb5960edc22d55209ac63d6b1bd7809bb53213d5
Dominick Baier <dbaier@leastprivilege.com>
2016-09-02 16:04:21 +0200

    update comments on consent controller

--- src/Host/Controllers/ConsentController.cs
+++ src/Host/Controllers/ConsentController.cs
@@ -42,7 +42,6 @@ namespace IdentityServer4.Quickstart.UI.Controllers
         [HttpGet]
         public async Task<IActionResult> Index(string returnUrl)
         {
-            // todo: check for valid return URL?
             var vm = await BuildViewModelAsync(returnUrl);
             if (vm != null)
             {
@@ -95,7 +94,6 @@ namespace IdentityServer4.Quickstart.UI.Controllers
                 // communicate outcome of consent back to identityserver
                 await _interaction.GrantConsentAsync(request, response);
 
-                // todo: check for valid return URL?
                 // redirect back to authorization endpoint
                 return Redirect(model.ReturnUrl);
             }
