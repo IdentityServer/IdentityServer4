41bbd03c831a2191a5401b541b9267546519e0c6
Brock Allen <brockallen@gmail.com>
2016-09-03 11:48:35 -0400

    missing license

--- src/IdentityServer4/Quickstart/Services/InMemoryLoginService.cs
+++ src/IdentityServer4/Quickstart/Services/InMemoryLoginService.cs
@@ -1,4 +1,7 @@
-﻿using System.Linq;
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+using System.Linq;
 using System.Collections.Generic;
 using System.Security.Claims;
 using System.IdentityModel.Tokens.Jwt;
