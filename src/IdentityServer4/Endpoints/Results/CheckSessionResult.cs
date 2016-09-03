04609f9b1e2d35cbbe53030dcf4f9637702f418a
Dominick Baier <dbaier@leastprivilege.com>
2016-09-03 09:00:40 +0200

    move event extensions

--- src/IdentityServer4/Endpoints/Results/CheckSessionResult.cs
+++ src/IdentityServer4/Endpoints/Results/CheckSessionResult.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using IdentityServer4.Hosting;
 using Microsoft.AspNetCore.Http;
-using IdentityServer4.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using IdentityServer4.Services;
 
