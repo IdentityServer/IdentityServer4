94cade731f7ffa7c0b15eee8614c67fdd072457a
Dominick Baier <dbaier@leastprivilege.com>
2016-09-12 17:13:16 +0200

    Added authentication properties support for authN manager extensions

--- src/IdentityServer4/Extensions/AuthenticationManagerExtensions.cs
+++ src/IdentityServer4/Extensions/AuthenticationManagerExtensions.cs
@@ -21,12 +21,28 @@ namespace Microsoft.AspNetCore.Http.Authentication
             await manager.SignInAsync(scheme, principal);
         }
 
+        public static async Task SignInAsync(this AuthenticationManager manager, string subject, string name, AuthenticationProperties properties, params Claim[] claims)
+        {
+            var scheme = manager.GetIdentityServerAuthenticationScheme();
+
+            var principal = IdentityServerPrincipal.Create(subject, name, claims);
+            await manager.SignInAsync(scheme, principal, properties);
+        }
+
         public static async Task SignInAsync(this AuthenticationManager manager, string subject, string name, string identityProvider, params Claim[] claims)
         {
             var scheme = manager.GetIdentityServerAuthenticationScheme();
 
             var principal = IdentityServerPrincipal.Create(subject, name, identityProvider, claims);
             await manager.SignInAsync(scheme, principal);
+        }
+
+        public static async Task SignInAsync(this AuthenticationManager manager, string subject, string name, string identityProvider, AuthenticationProperties properties, params Claim[] claims)
+        {
+            var scheme = manager.GetIdentityServerAuthenticationScheme();
+
+            var principal = IdentityServerPrincipal.Create(subject, name, identityProvider, claims);
+            await manager.SignInAsync(scheme, principal, properties);
         }
 
         public static async Task SignInAsync(this AuthenticationManager manager, string subject, string name, IEnumerable<string> authenticationMethods, params Claim[] claims)
@@ -37,12 +53,28 @@ namespace Microsoft.AspNetCore.Http.Authentication
             await manager.SignInAsync(scheme, principal);
         }
 
+        public static async Task SignInAsync(this AuthenticationManager manager, string subject, string name, IEnumerable<string> authenticationMethods, AuthenticationProperties properties, params Claim[] claims)
+        {
+            var scheme = manager.GetIdentityServerAuthenticationScheme();
+
+            var principal = IdentityServerPrincipal.Create(subject, name, authenticationMethods, claims);
+            await manager.SignInAsync(scheme, principal, properties);
+        }
+
         public static async Task SignInAsync(this AuthenticationManager manager, string sub, string name, string identityProvider, IEnumerable<string> authenticationMethods, params Claim[] claims)
         {
             var scheme = manager.GetIdentityServerAuthenticationScheme();
 
             var principal = IdentityServerPrincipal.Create(sub, name, identityProvider, authenticationMethods ,claims);
             await manager.SignInAsync(scheme, principal);
+        }
+
+        public static async Task SignInAsync(this AuthenticationManager manager, string sub, string name, string identityProvider, IEnumerable<string> authenticationMethods, AuthenticationProperties properties, params Claim[] claims)
+        {
+            var scheme = manager.GetIdentityServerAuthenticationScheme();
+
+            var principal = IdentityServerPrincipal.Create(sub, name, identityProvider, authenticationMethods, claims);
+            await manager.SignInAsync(scheme, principal, properties);
         }
 
         public static async Task SignOutAsync(this AuthenticationManager manager)
