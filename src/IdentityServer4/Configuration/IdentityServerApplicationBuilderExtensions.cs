d946249047f4e9092e332b8a4f6e0994e7edfcf9
Dominick Baier <dbaier@leastprivilege.com>
2016-09-14 12:51:45 +0200

    Started with logic for doing a startup service check

--- src/IdentityServer4/Configuration/IdentityServerApplicationBuilderExtensions.cs
+++ src/IdentityServer4/Configuration/IdentityServerApplicationBuilderExtensions.cs
@@ -3,6 +3,10 @@
 
 
 using IdentityServer4.Hosting;
+using IdentityServer4.Stores;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System;
 
 namespace Microsoft.AspNetCore.Builder
 {
@@ -10,6 +14,8 @@ namespace Microsoft.AspNetCore.Builder
     {
         public static IApplicationBuilder UseIdentityServer(this IApplicationBuilder app)
         {
+            app.Validate();
+
             app.UseMiddleware<BaseUrlMiddleware>();
 
             app.ConfigureCors();
@@ -18,6 +24,28 @@ namespace Microsoft.AspNetCore.Builder
             app.UseMiddleware<IdentityServerMiddleware>();
 
             return app;
+        }
+
+        internal static void Validate(this IApplicationBuilder app)
+        {
+            var loggerFactory = app.ApplicationServices.GetService(typeof(ILoggerFactory)) as ILoggerFactory;
+            var logger = loggerFactory.CreateLogger("IdentityServer4.Startup");
+
+            // todo: which other services to test for?
+            app.TestService(typeof(IPersistedGrantStore), logger, "No storage mechanism for grants specified. Use the 'AddInMemoryStores' extension method to register a development version.");
+        }
+
+        internal static void TestService(this IApplicationBuilder app, Type service, ILogger logger, string message = null)
+        {
+            var appService = app.ApplicationServices.GetService(service);
+
+            if (appService == null)
+            {
+                var error = message ?? $"Required service {service.GetType().FullName} is not registered in the DI container. Aborting startup";
+
+                logger.LogCritical(error);
+                throw new InvalidOperationException(error);
+            }
         }
     }
 }
