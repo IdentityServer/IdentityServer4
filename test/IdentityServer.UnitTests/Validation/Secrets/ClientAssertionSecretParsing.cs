f4f3d8f19b613c40c5739a20981e7ce372672b48
Yuriy Ostapenko <uncleyo@users.noreply.github.com>
2016-09-20 14:03:53 +0200

    Port "private_key_jwt" from IdentityServer3
    Full certificate X5c header support removed from the implementation for
    simplicity (since they are extremely rarely used and not well supported
    anyway).

---
+++ test/IdentityServer.UnitTests/Validation/Secrets/ClientAssertionSecretParsing.cs
@@ -0,0 +1,156 @@
+// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+using System.IdentityModel.Tokens.Jwt;
+using System.IO;
+using System.Text;
+using FluentAssertions;
+using IdentityServer4.Configuration;
+using IdentityServer4.UnitTests.Common;
+using IdentityServer4.Validation;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Xunit;
+
+namespace IdentityServer4.Tests.Validation.Secrets
+{
+    public class ClientAssertionSecretParsing
+    {
+        IdentityServerOptions _options;
+        ClientAssertionSecretParser _parser;
+
+        public ClientAssertionSecretParsing()
+        {
+            _options = new IdentityServerOptions();
+            _parser = new ClientAssertionSecretParser(_options, new LoggerFactory().CreateLogger<ClientAssertionSecretParser>());
+        }
+
+        [Fact]
+        public async void EmptyContext()
+        {
+            var context = new DefaultHttpContext();
+            context.Request.Body = new MemoryStream();
+            context.Request.ContentType = "application/x-www-form-urlencoded";
+
+            var secret = await _parser.ParseAsync(context);
+
+            secret.Should().BeNull();
+        }
+
+        [Fact]
+        public async void Valid_ClientAssertion()
+        {
+            var context = new DefaultHttpContext();
+
+            var body = "client_id=client&client_assertion_type=urn:ietf:params:oauth:client-assertion-type:jwt-bearer&client_assertion=token";
+
+            context.Request.Body = new MemoryStream(Encoding.UTF8.GetBytes(body));
+            context.Request.ContentType = "application/x-www-form-urlencoded";
+
+            var secret = await _parser.ParseAsync(context);
+
+            secret.Should().NotBeNull();
+            secret.Type.Should().Be(IdentityServerConstants.ParsedSecretTypes.JwtBearer);
+            secret.Id.Should().Be("client");
+            secret.Credential.Should().Be("token");
+        }
+
+        [Fact]
+        public async void Valid_ClientAssertion_ImplicitClientId()
+        {
+            var context = new DefaultHttpContext();
+
+            var token = new JwtSecurityToken(issuer: "client");
+            var tokenString = new JwtSecurityTokenHandler().WriteToken(token);
+
+            var body = "client_assertion_type=urn:ietf:params:oauth:client-assertion-type:jwt-bearer&client_assertion=" + tokenString;
+
+            context.Request.Body = new MemoryStream(Encoding.UTF8.GetBytes(body));
+            context.Request.ContentType = "application/x-www-form-urlencoded";
+
+            var secret = await _parser.ParseAsync(context);
+
+            secret.Should().NotBeNull();
+            secret.Type.Should().Be(IdentityServerConstants.ParsedSecretTypes.JwtBearer);
+            secret.Id.Should().Be("client");
+            secret.Credential.Should().Be(tokenString);
+        }
+
+        [Fact]
+        public async void Missing_ClientAssertionType()
+        {
+            var context = new DefaultHttpContext();
+
+            var body = "client_id=client&client_assertion=token";
+
+            context.Request.Body = new MemoryStream(Encoding.UTF8.GetBytes(body));
+            context.Request.ContentType = "application/x-www-form-urlencoded";
+
+            var secret = await _parser.ParseAsync(context);
+
+            secret.Should().BeNull();
+        }
+
+        [Fact]
+        public async void Missing_ClientAssertion()
+        {
+            var context = new DefaultHttpContext();
+
+            var body = "client_id=client&client_assertion_type=urn:ietf:params:oauth:client-assertion-type:jwt-bearer";
+
+            context.Request.Body = new MemoryStream(Encoding.UTF8.GetBytes(body));
+            context.Request.ContentType = "application/x-www-form-urlencoded";
+
+            var secret = await _parser.ParseAsync(context);
+
+            secret.Should().BeNull();
+        }
+
+        [Fact]
+        public async void Malformed_PostBody()
+        {
+            var context = new DefaultHttpContext();
+
+            var body = "malformed";
+
+            context.Request.Body = new MemoryStream(Encoding.UTF8.GetBytes(body));
+            context.Request.ContentType = "application/x-www-form-urlencoded";
+
+            var secret = await _parser.ParseAsync(context);
+
+            secret.Should().BeNull();
+        }
+
+        [Fact]
+        public async void ClientId_TooLong()
+        {
+            var context = new DefaultHttpContext();
+
+            var longClientId = "x".Repeat(_options.InputLengthRestrictions.ClientId + 1);
+            var body = $"client_id={longClientId}&client_assertion_type=urn:ietf:params:oauth:client-assertion-type:jwt-bearer&client_assertion=token";
+
+            context.Request.Body = new MemoryStream(Encoding.UTF8.GetBytes(body));
+            context.Request.ContentType = "application/x-www-form-urlencoded";
+
+            var secret = await _parser.ParseAsync(context);
+
+            secret.Should().BeNull();
+        }
+
+        [Fact]
+        public async void ClientAssertion_TooLong()
+        {
+            var context = new DefaultHttpContext();
+
+            var longToken = "x".Repeat(_options.InputLengthRestrictions.Jwt + 1);
+            var body = $"client_id=client&client_assertion_type=urn:ietf:params:oauth:client-assertion-type:jwt-bearer&client_assertion={longToken}";
+
+            context.Request.Body = new MemoryStream(Encoding.UTF8.GetBytes(body));
+            context.Request.ContentType = "application/x-www-form-urlencoded";
+
+            var secret = await _parser.ParseAsync(context);
+
+            secret.Should().BeNull();
+        }
+    }
+}
---
+++ test/IdentityServer.UnitTests/Validation/Secrets/PrivateKeyJwtSecretValidation.cs
@@ -0,0 +1,222 @@
+// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using FluentAssertions;
+using IdentityModel;
+using IdentityServer4.Configuration;
+using IdentityServer4.Models;
+using IdentityServer4.Stores;
+using IdentityServer4.Stores.InMemory;
+using IdentityServer4.UnitTests.Common;
+using IdentityServer4.UnitTests.Validation;
+using IdentityServer4.Validation;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using Xunit;
+
+namespace IdentityServer4.Tests.Validation.Secrets
+{
+    public class PrivateKeyJwtSecretValidation
+    {
+        readonly ISecretValidator _validator;
+        readonly IClientStore _clients;
+
+        public PrivateKeyJwtSecretValidation()
+        {
+            _validator = new PrivateKeyJwtSecretValidator(
+                new MockHttpContextAccessor(
+                    new IdentityServerOptions()
+                        {
+                            IssuerUri = "https://idsrv3.com"
+                        }
+                    ),
+                    new LoggerFactory().CreateLogger<PrivateKeyJwtSecretValidator>()
+                );
+            _clients = new InMemoryClientStore(ClientValidationTestClients.Get());
+        }
+
+        private JwtSecurityToken CreateToken(string clientId, DateTime? nowOverride = null)
+        {
+            var certificate = TestCert.Load();
+            var now = nowOverride ?? DateTime.Now;
+
+            var token = new JwtSecurityToken(
+                    clientId,
+                    "https://idsrv3.com/connect/token",
+                    new List<Claim>()
+                    {
+                        new Claim("jti", Guid.NewGuid().ToString()),
+                        new Claim(JwtClaimTypes.Subject, clientId),
+                        new Claim(JwtClaimTypes.IssuedAt, now.ToEpochTime().ToString(), ClaimValueTypes.Integer64)
+                    },
+                    now,
+                    now.AddMinutes(1),
+                    new SigningCredentials(
+                        new X509SecurityKey(certificate),
+                        SecurityAlgorithms.RsaSha256
+                    )
+                );
+            return token;
+        }
+
+        [Fact]
+        public async Task Invalid_Certificate_X5t_Only_Requires_Full_Certificate()
+        {
+            var clientId = "certificate_valid";
+            var client = await _clients.FindClientByIdAsync(clientId);
+
+            var token = CreateToken(clientId);
+            var secret = new ParsedSecret
+            {
+                Id = clientId,
+                Credential = new JwtSecurityTokenHandler().WriteToken(token),
+                Type = IdentityServerConstants.ParsedSecretTypes.JwtBearer
+            };
+
+            var result = await _validator.ValidateAsync(client.ClientSecrets, secret);
+
+            result.Success.Should().BeFalse();
+        }
+
+        [Fact]
+        public async Task Invalid_Certificate_Thumbprint()
+        {
+            var clientId = "certificate_invalid";
+            var client = await _clients.FindClientByIdAsync(clientId);
+
+            var secret = new ParsedSecret
+            {
+                Id = clientId,
+                Credential = new JwtSecurityTokenHandler().WriteToken(CreateToken(clientId)),
+                Type = IdentityServerConstants.ParsedSecretTypes.JwtBearer
+            };
+
+            var result = await _validator.ValidateAsync(client.ClientSecrets, secret);
+
+            result.Success.Should().BeFalse();
+        }
+
+        [Fact]
+        public async Task Valid_Certificate_Base64()
+        {
+            var clientId = "certificate_base64_valid";
+            var client = await _clients.FindClientByIdAsync(clientId);
+
+            var secret = new ParsedSecret
+            {
+                Id = clientId,
+                Credential = new JwtSecurityTokenHandler().WriteToken(CreateToken(clientId)),
+                Type = IdentityServerConstants.ParsedSecretTypes.JwtBearer
+            };
+
+            var result = await _validator.ValidateAsync(client.ClientSecrets, secret);
+
+            result.Success.Should().BeTrue();
+        }
+
+        [Fact]
+        public async Task Invalid_Certificate_Base64()
+        {
+            var clientId = "certificate_base64_invalid";
+            var client = await _clients.FindClientByIdAsync(clientId);
+
+            var secret = new ParsedSecret
+            {
+                Id = clientId,
+                Credential = new JwtSecurityTokenHandler().WriteToken(CreateToken(clientId)),
+                Type = IdentityServerConstants.ParsedSecretTypes.JwtBearer
+            };
+
+            var result = await _validator.ValidateAsync(client.ClientSecrets, secret);
+
+            result.Success.Should().BeFalse();
+        }
+
+        [Fact]
+        public async Task Invalid_Issuer()
+        {
+            var clientId = "certificate_valid";
+            var client = await _clients.FindClientByIdAsync(clientId);
+
+            var token = CreateToken(clientId);
+            token.Payload.Remove(JwtClaimTypes.Issuer);
+            token.Payload.Add(JwtClaimTypes.Issuer, "invalid");
+            var secret = new ParsedSecret
+            {
+                Id = clientId,
+                Credential = new JwtSecurityTokenHandler().WriteToken(token),
+                Type = IdentityServerConstants.ParsedSecretTypes.JwtBearer
+            };
+
+            var result = await _validator.ValidateAsync(client.ClientSecrets, secret);
+
+            result.Success.Should().BeFalse();
+        }
+
+        [Fact]
+        public async Task Invalid_Subject()
+        {
+            var clientId = "certificate_valid";
+            var client = await _clients.FindClientByIdAsync(clientId);
+
+            var token = CreateToken(clientId);
+            token.Payload.Remove(JwtClaimTypes.Subject);
+            token.Payload.Add(JwtClaimTypes.Subject, "invalid");
+            var secret = new ParsedSecret
+            {
+                Id = clientId,
+                Credential = new JwtSecurityTokenHandler().WriteToken(token),
+                Type = IdentityServerConstants.ParsedSecretTypes.JwtBearer
+            };
+
+            var result = await _validator.ValidateAsync(client.ClientSecrets, secret);
+
+            result.Success.Should().BeFalse();
+        }
+
+        [Fact]
+        public async Task Invalid_Expired_Token()
+        {
+            var clientId = "certificate_valid";
+            var client = await _clients.FindClientByIdAsync(clientId);
+
+            var token = CreateToken(clientId, DateTime.Now.AddHours(-1));
+            var secret = new ParsedSecret
+            {
+                Id = clientId,
+                Credential = new JwtSecurityTokenHandler().WriteToken(token),
+                Type = IdentityServerConstants.ParsedSecretTypes.JwtBearer
+            };
+
+            var result = await _validator.ValidateAsync(client.ClientSecrets, secret);
+
+            result.Success.Should().BeFalse();
+        }
+
+        [Fact]
+        public async Task Invalid_Unsigned_Token()
+        {
+            var clientId = "certificate_valid";
+            var client = await _clients.FindClientByIdAsync(clientId);
+
+            var token = CreateToken(clientId);
+            token.Header.Remove("alg");
+            token.Header.Add("alg", "none");
+            var secret = new ParsedSecret
+            {
+                Id = clientId,
+                Credential = new JwtSecurityTokenHandler().WriteToken(token),
+                Type = IdentityServerConstants.ParsedSecretTypes.JwtBearer
+            };
+
+            var result = await _validator.ValidateAsync(client.ClientSecrets, secret);
+
+            result.Success.Should().BeFalse();
+        }
+    }
+}
