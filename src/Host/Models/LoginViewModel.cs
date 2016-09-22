ce1a4c6c5d366dee4cdca92fd36cd9b9d2942d52
Brock Allen <brockallen@gmail.com>
2016-09-22 13:49:12 -0400

    use client config on login page (enable local; allowed providers) (#321)

--- src/Host/Models/LoginViewModel.cs
+++ src/Host/Models/LoginViewModel.cs
@@ -3,32 +3,12 @@
 
 
 using System.Collections.Generic;
-using Microsoft.AspNetCore.Http;
-using System.Linq;
 
 namespace IdentityServer4.Quickstart.UI.Models
 {
     public class LoginViewModel : LoginInputModel
     {
-        public LoginViewModel(HttpContext httpContext)
-        {
-            ExternalProviders = httpContext.Authentication.GetAuthenticationSchemes()
-                .Where(x => x.DisplayName != null)
-                .Select(x => new ExternalProvider {
-                    DisplayName = x.DisplayName,
-                    AuthenticationScheme = x.AuthenticationScheme
-                });
-        }
-
-        public LoginViewModel(HttpContext httpContext, LoginInputModel other)
-            : this(httpContext)
-        {
-            Username = other.Username;
-            Password = other.Password;
-            ReturnUrl = other.ReturnUrl;
-        }
-
-        public string ErrorMessage { get; set; }
+        public bool EnableLocalLogin { get; set; }
         public IEnumerable<ExternalProvider> ExternalProviders { get; set; }
     }
 
