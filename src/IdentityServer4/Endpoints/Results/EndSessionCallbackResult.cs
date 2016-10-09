9d24da84f8c962911b421c4b40bc0fea0a25521c
Brock Allen <brockallen@gmail.com>
2016-10-09 12:23:32 -0400

    add XFO/CSP to end session callback result

--- src/IdentityServer4/Endpoints/Results/EndSessionCallbackResult.cs
+++ src/IdentityServer4/Endpoints/Results/EndSessionCallbackResult.cs
@@ -13,6 +13,8 @@ using IdentityServer4.Stores;
 using IdentityServer4.Models;
 using System.Net;
 using System;
+using IdentityServer4.Extensions;
+using IdentityServer4.Configuration;
 
 namespace IdentityServer4.Endpoints.Results
 {
@@ -31,23 +33,27 @@ namespace IdentityServer4.Endpoints.Results
             EndSessionCallbackValidationResult result,
             ISessionIdService sessionId,
             IClientSessionService clientList,
-            IMessageStore<LogoutMessage> logoutMessageStore)
+            IMessageStore<LogoutMessage> logoutMessageStore,
+            IdentityServerOptions options)
             : this(result)
         {
             _sessionId = sessionId;
             _clientList = clientList;
             _logoutMessageStore = logoutMessageStore;
+            _options = options;
         }
 
         private ISessionIdService _sessionId;
         private IClientSessionService _clientList;
         private IMessageStore<LogoutMessage> _logoutMessageStore;
+        private IdentityServerOptions _options;
 
         void Init(HttpContext context)
         {
             _sessionId = _sessionId ?? context.RequestServices.GetRequiredService<ISessionIdService>();
             _clientList = _clientList ?? context.RequestServices.GetRequiredService<IClientSessionService>();
             _logoutMessageStore = _logoutMessageStore ?? context.RequestServices.GetRequiredService<IMessageStore<LogoutMessage>>();
+            _options = _options ?? context.RequestServices.GetRequiredService<IdentityServerOptions>();
         }
 
         public async Task ExecuteAsync(HttpContext context)
@@ -69,9 +75,48 @@ namespace IdentityServer4.Endpoints.Results
                 _clientList.RemoveCookie();
 
                 context.Response.SetNoCache();
+                AddXfoHeaders(context);
+                AddCspHeaders(context);
 
                 var html = GetHtml();
                 await context.Response.WriteHtmlAsync(html);
+            }
+        }
+
+        private void AddCspHeaders(HttpContext context)
+        {
+            var value = "default-src 'none'";
+            var origins = _result.ClientLogoutUrls?.Select(x => x.GetOrigin()).Aggregate((x, y) => $"{x} {y}");
+            if (origins != null)
+            {
+                // this hash matches the style being used below for the iframe
+                value += $";style-src 'sha256-u+OupXgfekP+x/f6rMdoEAspPCYUtca912isERnoEjY=';frame-src {origins}";
+            }
+
+            if (!context.Response.Headers.ContainsKey("Content-Security-Policy"))
+            {
+                context.Response.Headers.Add("Content-Security-Policy", value);
+            }
+
+            if (!context.Response.Headers.ContainsKey("X-Content-Security-Policy"))
+            {
+                context.Response.Headers.Add("X-Content-Security-Policy", value);
+            }
+        }
+
+        private void AddXfoHeaders(HttpContext context)
+        {
+            if (!context.Response.Headers.ContainsKey("X-Frame-Options"))
+            {
+                var logoutPageUrl = _options.UserInteractionOptions.LogoutUrl;
+                if (logoutPageUrl.IsLocalUrl())
+                {
+                    context.Response.Headers.Add("X-Frame-Options", "SAMEORIGIN");
+                }
+                else
+                {
+                    context.Response.Headers.Add("X-Frame-Options", $"ALLOWFROM {logoutPageUrl.GetOrigin()}");
+                }
             }
         }
 
@@ -81,11 +126,11 @@ namespace IdentityServer4.Endpoints.Results
 
             if (_result.ClientLogoutUrls != null && _result.ClientLogoutUrls.Any())
             {
-                var frameUrls = _result.ClientLogoutUrls.Select(url => $"<iframe style='display:none' width='0' height='0' src='{url}'></iframe>");
+                var frameUrls = _result.ClientLogoutUrls.Select(url => $"<iframe src='{url}'></iframe>");
                 framesHtml = frameUrls.Aggregate((x, y) => x + y);
             }
 
-            return $"<!DOCTYPE html><html><body>{framesHtml}</body></html>";
+            return $"<!DOCTYPE html><html><style>iframe{{display:none;width:0;height:0;}}</style><body>{framesHtml}</body></html>";
         }
     }
 }
