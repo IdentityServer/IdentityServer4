6138d728a7cb672067dbdf4216d6e1a6fb76f484
Brock Allen <brockallen@gmail.com>
2016-12-02 15:37:34 -0500

    tests for default claims service

--- test/IdentityServer.UnitTests/Common/MockProfileService.cs
+++ test/IdentityServer.UnitTests/Common/MockProfileService.cs
@@ -5,18 +5,34 @@
 using IdentityServer4.Services;
 using System.Threading.Tasks;
 using IdentityServer4.Models;
+using System.Security.Claims;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace IdentityServer4.UnitTests.Common
 {
     public class MockProfileService : IProfileService
     {
+        public ICollection<Claim> ProfileClaims { get; set; } = new HashSet<Claim>();
+        public bool IsActive { get; set; } = true;
+
+        public bool GetProfileWasCalled => ProfileContext != null;
+        public ProfileDataRequestContext ProfileContext { get; set; }
+
+        public bool IsActiveWasCalled => ActiveContext != null;
+        public IsActiveContext ActiveContext { get; set; }
+
         public Task GetProfileDataAsync(ProfileDataRequestContext context)
         {
+            ProfileContext = context;
+            context.IssuedClaims = ProfileClaims.ToList();
             return Task.FromResult(0);
         }
 
         public Task IsActiveAsync(IsActiveContext context)
         {
+            ActiveContext = context;
+            context.IsActive = IsActive;
             return Task.FromResult(0);
         }
     }
