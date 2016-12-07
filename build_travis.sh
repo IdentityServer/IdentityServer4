8a1b7f220961f26e1d611daace9f6d5fd4405a67
Dominick Baier <dbaier@leastprivilege.com>
2016-12-07 10:57:33 +0100

    switch to declarative dotnet for travis

--- build_travis.sh
+++ build_travis.sh
@@ -42,19 +42,19 @@ fi
 # INSTALL .NET CORE CLI
 ###########################################################################
 
-echo "Installing .NET CLI..."
-if [ ! -d "$SCRIPT_DIR/.dotnet" ]; then
-  mkdir "$SCRIPT_DIR/.dotnet"
-fi
-curl -Lsfo "$SCRIPT_DIR/.dotnet/dotnet-install.sh" https://raw.githubusercontent.com/dotnet/cli/rel/1.0.0-preview2/scripts/obtain/dotnet-install.sh
-sudo bash "$SCRIPT_DIR/.dotnet/dotnet-install.sh" --install-dir .dotnet --no-path
+#echo "Installing .NET CLI..."
+#if [ ! -d "$SCRIPT_DIR/.dotnet" ]; then
+#  mkdir "$SCRIPT_DIR/.dotnet"
+#fi
+#curl -Lsfo "$SCRIPT_DIR/.dotnet/dotnet-install.sh" https://raw.githubusercontent.com/dotnet/cli/rel/1.0.0-preview2/scripts/obtain/dotnet-install.sh
+#sudo bash "$SCRIPT_DIR/.dotnet/dotnet-install.sh" --install-dir .dotnet --no-path
 # sudo bash "$SCRIPT_DIR/.dotnet/dotnet-install.sh" --version 1.0.0-preview2-003121 --install-dir .dotnet --no-path
-export PATH="$SCRIPT_DIR/.dotnet":$PATH
-export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
-export DOTNET_CLI_TELEMETRY_OPTOUT=1
+#export PATH="$SCRIPT_DIR/.dotnet":$PATH
+#export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
+#export DOTNET_CLI_TELEMETRY_OPTOUT=1
 
-"$SCRIPT_DIR/.dotnet/dotnet"
-"$SCRIPT_DIR/.dotnet/dotnet" --info
+#"$SCRIPT_DIR/.dotnet/dotnet"
+#"$SCRIPT_DIR/.dotnet/dotnet" --info
 
 ###########################################################################
 # INSTALL NUGET
