71bc723be296e7b71b28e3d4ab64d72436a25034
Brock Allen <brockallen@gmail.com>
2016-12-22 09:38:36 -0500

    license headers

--- src/IdentityServer4/Extensions/IdentityServerToolsExtensions.cs
+++ src/IdentityServer4/Extensions/IdentityServerToolsExtensions.cs
@@ -1,4 +1,8 @@
-﻿using IdentityModel;
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityModel;
 using System.Collections.Generic;
 using System.Security.Claims;
 using System.Threading.Tasks;
--- src/IdentityServer4/Infrastructure/NameValueCollectionConverter.cs
+++ src/IdentityServer4/Infrastructure/NameValueCollectionConverter.cs
@@ -1,4 +1,8 @@
-﻿using Newtonsoft.Json;
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using Newtonsoft.Json;
 using System;
 using System.Collections.Specialized;
 
