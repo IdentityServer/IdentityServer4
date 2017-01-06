495a42b077bc28962142d87b6a878ef1d455345d
Dominick Baier <dbaier@leastprivilege.com>
2017-01-06 15:17:40 +0100

    added tests to validate #632

--- test/IdentityServer.IntegrationTests/Clients/Setup/Clients.cs
+++ test/IdentityServer.IntegrationTests/Clients/Setup/Clients.cs
@@ -36,6 +36,22 @@ namespace IdentityServer4.IntegrationTests.Clients
                 },
                 new Client
                 {
+                    ClientId = "client.and.ro",
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPasswordAndClientCredentials,
+
+                    AllowedScopes =
+                    {
+                        "openid",
+                        "api1", "api2"
+                    }
+                },
+                new Client
+                {
                     ClientId = "client.identityscopes",
                     ClientSecrets =
                     {
