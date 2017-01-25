86f98efaffc3f35734897658b1b553ed29a8be2f
Dominick Baier <dbaier@leastprivilege.com>
2017-01-25 18:07:22 +0100

    return id_token on refresh token request (see #690) (#694)

---
+++ test/IdentityServer.IntegrationTests/Clients/RefreshTokenClient.cs
@@ -0,0 +1,58 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using FluentAssertions;
+using IdentityModel.Client;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace IdentityServer4.IntegrationTests.Clients
+{
+    public class RefreshTokenClient
+    {
+        const string TokenEndpoint = "https://server/connect/token";
+
+        private readonly HttpClient _client;
+        private readonly HttpMessageHandler _handler;
+
+        public RefreshTokenClient()
+        {
+            var builder = new WebHostBuilder()
+                .UseStartup<Startup>();
+            var server = new TestServer(builder);
+
+            _handler = server.CreateHandler();
+            _client = server.CreateClient();
+        }
+
+        [Fact]
+        public async Task requesting_a_refresh_token_should_return_expected_results()
+        {
+            var client = new TokenClient(
+                TokenEndpoint,
+                "roclient",
+                "secret",
+                innerHttpMessageHandler: _handler);
+
+            var response = await client.RequestResourceOwnerPasswordAsync("bob", "bob", "api1 offline_access");
+
+            response.IsError.Should().BeFalse();
+            response.ExpiresIn.Should().Be(3600);
+            response.TokenType.Should().Be("Bearer");
+            response.IdentityToken.Should().BeNull();
+            response.RefreshToken.Should().NotBeNull();
+
+            response = await client.RequestRefreshTokenAsync(response.RefreshToken);
+
+            response.IsError.Should().BeFalse();
+            response.ExpiresIn.Should().Be(3600);
+            response.TokenType.Should().Be("Bearer");
+            response.IdentityToken.Should().NotBeNull();
+            response.RefreshToken.Should().NotBeNull();
+        }
+    }
+}
