a388a908bb9624b71f20f9183187a28bc7922ea1
user1336 <h2354067@mvrht.com>
2017-02-16 12:19:54 +0100

    Changed TestService so it will not get the services from the Root ServiceProvider so it can work with Scoped services

--- src/IdentityServer4/Configuration/IdentityServerApplicationBuilderExtensions.cs
+++ src/IdentityServer4/Configuration/IdentityServerApplicationBuilderExtensions.cs
@@ -5,6 +5,7 @@
 using IdentityServer4.Hosting;
 using IdentityServer4.Stores;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 
 namespace Microsoft.AspNetCore.Builder
@@ -27,25 +28,28 @@ namespace Microsoft.AspNetCore.Builder
 
         internal static void Validate(this IApplicationBuilder app)
         {
+            var scopeFactory = app.ApplicationServices.GetService<IServiceScopeFactory>();
+            var serviceProvider = scopeFactory.CreateScope().ServiceProvider;
+
             var loggerFactory = app.ApplicationServices.GetService(typeof(ILoggerFactory)) as ILoggerFactory;
             if (loggerFactory == null) throw new ArgumentNullException(nameof(loggerFactory));
 
             var logger = loggerFactory.CreateLogger("IdentityServer4.Startup");
 
-            app.TestService(typeof(IPersistedGrantStore), logger, "No storage mechanism for grants specified. Use the 'AddInMemoryPersistedGrants' extension method to register a development version.");
-            app.TestService(typeof(IClientStore), logger, "No storage mechanism for clients specified. Use the 'AddInMemoryClients' extension method to register a development version.");
-            app.TestService(typeof(IResourceStore), logger, "No storage mechanism for resources specified. Use the 'AddInMemoryResources' extension method to register a development version.");
+            TestService(serviceProvider, typeof(IPersistedGrantStore), logger, "No storage mechanism for grants specified. Use the 'AddInMemoryPersistedGrants' extension method to register a development version.");
+            TestService(serviceProvider, typeof(IClientStore), logger, "No storage mechanism for clients specified. Use the 'AddInMemoryClients' extension method to register a development version.");
+            TestService(serviceProvider, typeof(IResourceStore), logger, "No storage mechanism for resources specified. Use the 'AddInMemoryResources' extension method to register a development version.");
 
-            var persistedGrants = app.ApplicationServices.GetService(typeof(IPersistedGrantStore));
+            var persistedGrants = serviceProvider.GetService(typeof(IPersistedGrantStore));
             if (persistedGrants.GetType().FullName == typeof(InMemoryPersistedGrantStore).FullName)
             {
                 logger.LogInformation("You are using the in-memory version of the persisted grant store. This will store consent decisions, authorization codes, refresh and reference tokens in memory only. If you are using any of those features in production, you want to switch to a different store implementation.");
             }
         }
 
-        internal static object TestService(this IApplicationBuilder app, Type service, ILogger logger, string message = null, bool doThrow = true)
+        internal static object TestService(IServiceProvider serviceProvider, Type service, ILogger logger, string message = null, bool doThrow = true)
         {
-            var appService = app.ApplicationServices.GetService(service);
+            var appService = serviceProvider.GetService(service);
 
             if (appService == null)
             {
