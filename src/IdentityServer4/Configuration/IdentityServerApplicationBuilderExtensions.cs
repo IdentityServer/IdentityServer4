28e12ea9ecba59058e002317b823489894d8a1c7
user1336 <h2354067@mvrht.com>
2017-02-22 08:43:02 +0100

    Call Dispose on the ServiceScope

--- src/IdentityServer4/Configuration/IdentityServerApplicationBuilderExtensions.cs
+++ src/IdentityServer4/Configuration/IdentityServerApplicationBuilderExtensions.cs
@@ -28,22 +28,26 @@ namespace Microsoft.AspNetCore.Builder
 
         internal static void Validate(this IApplicationBuilder app)
         {
-            var scopeFactory = app.ApplicationServices.GetService<IServiceScopeFactory>();
-            var serviceProvider = scopeFactory.CreateScope().ServiceProvider;
-
             var loggerFactory = app.ApplicationServices.GetService(typeof(ILoggerFactory)) as ILoggerFactory;
             if (loggerFactory == null) throw new ArgumentNullException(nameof(loggerFactory));
 
             var logger = loggerFactory.CreateLogger("IdentityServer4.Startup");
 
-            TestService(serviceProvider, typeof(IPersistedGrantStore), logger, "No storage mechanism for grants specified. Use the 'AddInMemoryPersistedGrants' extension method to register a development version.");
-            TestService(serviceProvider, typeof(IClientStore), logger, "No storage mechanism for clients specified. Use the 'AddInMemoryClients' extension method to register a development version.");
-            TestService(serviceProvider, typeof(IResourceStore), logger, "No storage mechanism for resources specified. Use the 'AddInMemoryResources' extension method to register a development version.");
+            var scopeFactory = app.ApplicationServices.GetService<IServiceScopeFactory>();
 
-            var persistedGrants = serviceProvider.GetService(typeof(IPersistedGrantStore));
-            if (persistedGrants.GetType().FullName == typeof(InMemoryPersistedGrantStore).FullName)
+            using (var scope = scopeFactory.CreateScope())
             {
-                logger.LogInformation("You are using the in-memory version of the persisted grant store. This will store consent decisions, authorization codes, refresh and reference tokens in memory only. If you are using any of those features in production, you want to switch to a different store implementation.");
+                var serviceProvider = scope.ServiceProvider;
+
+                TestService(serviceProvider, typeof(IPersistedGrantStore), logger, "No storage mechanism for grants specified. Use the 'AddInMemoryPersistedGrants' extension method to register a development version.");
+                TestService(serviceProvider, typeof(IClientStore), logger, "No storage mechanism for clients specified. Use the 'AddInMemoryClients' extension method to register a development version.");
+                TestService(serviceProvider, typeof(IResourceStore), logger, "No storage mechanism for resources specified. Use the 'AddInMemoryResources' extension method to register a development version.");
+
+                var persistedGrants = serviceProvider.GetService(typeof(IPersistedGrantStore));
+                if (persistedGrants.GetType().FullName == typeof(InMemoryPersistedGrantStore).FullName)
+                {
+                    logger.LogInformation("You are using the in-memory version of the persisted grant store. This will store consent decisions, authorization codes, refresh and reference tokens in memory only. If you are using any of those features in production, you want to switch to a different store implementation.");
+                }
             }
         }
 
