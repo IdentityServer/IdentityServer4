6237278887c4886a9f66008e254cfcb5eea59d1d
Brock Allen <brockallen@gmail.com>
2017-03-04 16:21:52 -0500

    filter external idps without caption in view

--- src/Host/Quickstart/Account/LoginViewModel.cs
+++ src/Host/Quickstart/Account/LoginViewModel.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -11,8 +12,11 @@ namespace IdentityServer4.Quickstart.UI
     {
         public bool AllowRememberLogin { get; set; }
         public bool EnableLocalLogin { get; set; }
+
         public IEnumerable<ExternalProvider> ExternalProviders { get; set; }
+        public IEnumerable<ExternalProvider> VisibleExternalProviders => ExternalProviders.Where(x => !String.IsNullOrWhiteSpace(x.DisplayName));
 
         public bool IsExternalLoginOnly => EnableLocalLogin == false && ExternalProviders?.Count() == 1;
+        public string ExternalLoginScheme => ExternalProviders?.SingleOrDefault()?.AuthenticationScheme;
     }
 }
