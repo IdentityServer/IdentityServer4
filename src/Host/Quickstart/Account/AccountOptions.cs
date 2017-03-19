47a288927311c13af8d3ec135e8a12eb48e859bc
Brock Allen <brockallen@gmail.com>
2017-03-19 17:54:43 -0400

    disable windows by default and add a note about IIS requiring configuration

--- src/Host/Quickstart/Account/AccountOptions.cs
+++ src/Host/Quickstart/Account/AccountOptions.cs
@@ -15,7 +15,9 @@ namespace IdentityServer4.Quickstart.UI
         public static bool ShowLogoutPrompt = true;
         public static bool AutomaticRedirectAfterSignOut = false;
 
-        public static bool WindowsAuthenticationEnabled = true;
+        // to enable windows authentication, the host (IIS or IIS Express) also must have 
+        // windows auth enabled.
+        public static bool WindowsAuthenticationEnabled = false;
         // specify the Windows authentication schemes you want to use for authentication
         public static readonly string[] WindowsAuthenticationSchemes = new string[] { "Negotiate", "NTLM" };
         public static readonly string WindowsAuthenticationProviderName = "Windows";
