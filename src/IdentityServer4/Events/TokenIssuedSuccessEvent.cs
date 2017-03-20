f91c5e5d86d2d819a40b97746f1140523a6940f4
Brock Allen <brockallen@gmail.com>
2017-03-20 14:57:56 -0400

    moar

--- src/IdentityServer4/Events/TokenIssuedSuccessEvent.cs
+++ src/IdentityServer4/Events/TokenIssuedSuccessEvent.cs
@@ -3,13 +3,17 @@ namespace IdentityServer4.Events
 {
     public class TokenIssuedSuccessEvent : Event
     {
-        public TokenIssuedSuccessEvent()
+        public TokenIssuedSuccessEvent(string grantType, string tokenType)
             : base(EventCategories.Token, 
                   "Token Issued Success",
                   EventTypes.Success, 
                   EventIds.TokenIssuedSuccess)
         {
+            GrantType = grantType;
+            TokenType = tokenType;
         }
 
+        public string GrantType { get; set; }
+        public string TokenType { get; set; }
     }
 }
