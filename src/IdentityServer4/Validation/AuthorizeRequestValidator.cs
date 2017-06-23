5c9e211a628d8c2397c472a25aab53f803fb58bb
Dominick Baier <dbaier@leastprivilege.com>
2017-06-23 12:37:17 +0200

    Feature: Allow PKCE on demand (#1270)
    * change authorize request validator to accept PKCE without explicit setting
    
    * Add test to verify optional authorize request
    
    * Added more tests around optional PKCE
    
    * Add tests for token request validator
    
    * Update PKCE conformance tests

--- src/IdentityServer4/Validation/AuthorizeRequestValidator.cs
+++ src/IdentityServer4/Validation/AuthorizeRequestValidator.cs
@@ -241,18 +241,16 @@ namespace IdentityServer4.Validation
             //////////////////////////////////////////////////////////
             if (request.GrantType == GrantType.AuthorizationCode || request.GrantType == GrantType.Hybrid)
             {
-                if (request.Client.RequirePkce)
+                _logger.LogDebug("Checking for PKCE parameters");
+                
+                /////////////////////////////////////////////////////////////////////////////
+                // validate code_challenge and code_challenge_method
+                /////////////////////////////////////////////////////////////////////////////
+                var proofKeyResult = ValidatePkceParameters(request);
+                
+                if (proofKeyResult.IsError)
                 {
-                    _logger.LogDebug("Client requires a proof key for code exchange. Starting PKCE validation");
-
-                    /////////////////////////////////////////////////////////////////////////////
-                    // validate code_challenge and code_challenge_method
-                    /////////////////////////////////////////////////////////////////////////////
-                    var proofKeyResult = ValidatePkceParameters(request);
-                    if (proofKeyResult.IsError)
-                    {
-                        return proofKeyResult;
-                    }
+                    return proofKeyResult;
                 }
             }
 
@@ -317,8 +315,17 @@ namespace IdentityServer4.Validation
             var codeChallenge = request.Raw.Get(OidcConstants.AuthorizeRequest.CodeChallenge);
             if (codeChallenge.IsMissing())
             {
-                LogError("code_challenge is missing", request);
-                fail.ErrorDescription = "code challenge required";
+                if (request.Client.RequirePkce)
+                {
+                    LogError("code_challenge is missing", request);
+                    fail.ErrorDescription = "code challenge required";
+                }
+                else
+                {
+                    _logger.LogDebug("No PKCE used.");
+                    return Valid(request);
+                }
+                
                 return fail;
             }
 
