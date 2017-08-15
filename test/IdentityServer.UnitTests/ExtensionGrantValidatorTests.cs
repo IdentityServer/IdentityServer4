aa7074c358c61a3636c2f4e43fc24b8110cd0080
duyvan82 <van.vothanh@outlook.com>
2017-08-15 11:37:51 +1000

    include username in ExtensionGrantValidationContext

---
+++ test/IdentityServer.UnitTests/ExtensionGrantValidatorTests.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Specialized;
+using System.Threading.Tasks;
+using FluentAssertions;
+using IdentityServer4.UnitTests.Common;
+using IdentityServer4.Validation;
+using Xunit;
+
+namespace IdentityServer4.UnitTests.Validation
+{
+    public class ExtensionGrantValidatorTests
+    {
+        private readonly ExtensionGrantValidator _sut;
+
+        public ExtensionGrantValidatorTests()
+        {
+            _sut = new ExtensionGrantValidator(new[] {new TestGrantValidator(true)}, TestLogger.Create<ExtensionGrantValidator>());
+        }
+
+        [Fact]
+        public async Task should_populate_username_property_in_extension_grant_validation_context()
+        {
+            var request = new ValidatedTokenRequest
+            {
+                Raw = new NameValueCollection
+                {
+                    { "username", "12345678" }
+                },
+                GrantType = "custom_grant"
+            };
+
+            var result = await _sut.ValidateAsync(request);
+
+            result.Should().NotBeNull();
+            request.UserName.Should().Be("12345678");
+        }
+    }
+}
