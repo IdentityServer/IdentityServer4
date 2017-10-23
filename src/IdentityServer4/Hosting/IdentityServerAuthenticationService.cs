de4c0524b7637487fb72295e5151bfdfdc1ad6b3
Dominick Baier <dbaier@leastprivilege.com>
2017-10-23 10:25:26 +0200

    Obsolete old methods and move internals to authentication service

--- src/IdentityServer4/Hosting/IdentityServerAuthenticationService.cs
+++ src/IdentityServer4/Hosting/IdentityServerAuthenticationService.cs
@@ -11,6 +11,8 @@ using Microsoft.Extensions.Logging;
 using IdentityServer4.Configuration.DependencyInjection;
 using IdentityServer4.Extensions;
 using System;
+using IdentityModel;
+using System.Linq;
 
 namespace IdentityServer4.Hosting
 {
@@ -74,8 +76,8 @@ namespace IdentityServer4.Hosting
         {
             _logger.LogDebug("Augmenting SignInContext");
 
-            principal.AssertRequiredClaims();
-            principal.AugmentMissingClaims(_clock.UtcNow.UtcDateTime);
+            AssertRequiredClaims(principal);
+            AugmentMissingClaims(principal, _clock.UtcNow.UtcDateTime);
         }
 
         public async Task SignOutAsync(HttpContext context, string scheme, AuthenticationProperties properties)
@@ -108,6 +110,47 @@ namespace IdentityServer4.Hosting
         public Task ForbidAsync(HttpContext context, string scheme, AuthenticationProperties properties)
         {
             return _inner.ForbidAsync(context, scheme, properties);
+        }
+
+        private void AssertRequiredClaims(ClaimsPrincipal principal)
+        {
+            // for now, we don't allow more than one identity in the principal/cookie
+            if (principal.Identities.Count() != 1) throw new InvalidOperationException("only a single identity supported");
+            if (principal.FindFirst(JwtClaimTypes.Subject) == null) throw new InvalidOperationException("sub claim is missing");
+        }
+
+        private void AugmentMissingClaims(ClaimsPrincipal principal, DateTime authTime)
+        {
+            var identity = principal.Identities.First();
+
+            // ASP.NET Identity issues this claim type and uses the authentication middleware name
+            // such as "Google" for the value. this code is trying to correct/convert that for
+            // our scenario. IOW, we take their old AuthenticationMethod value of "Google"
+            // and issue it as the idp claim. we then also issue a amr with "external"
+            var amr = identity.FindFirst(ClaimTypes.AuthenticationMethod);
+            if (amr != null &&
+                identity.FindFirst(JwtClaimTypes.IdentityProvider) == null &&
+                identity.FindFirst(JwtClaimTypes.AuthenticationMethod) == null)
+            {
+                identity.RemoveClaim(amr);
+                identity.AddClaim(new Claim(JwtClaimTypes.IdentityProvider, amr.Value));
+                identity.AddClaim(new Claim(JwtClaimTypes.AuthenticationMethod, Constants.ExternalAuthenticationMethod));
+            }
+
+            if (identity.FindFirst(JwtClaimTypes.IdentityProvider) == null)
+            {
+                identity.AddClaim(new Claim(JwtClaimTypes.IdentityProvider, IdentityServerConstants.LocalIdentityProvider));
+            }
+
+            if (identity.FindFirst(JwtClaimTypes.AuthenticationMethod) == null)
+            {
+                identity.AddClaim(new Claim(JwtClaimTypes.AuthenticationMethod, OidcConstants.AuthenticationMethods.Password));
+            }
+
+            if (identity.FindFirst(JwtClaimTypes.AuthenticationTime) == null)
+            {
+                identity.AddClaim(new Claim(JwtClaimTypes.AuthenticationTime, authTime.ToEpochTime().ToString(), ClaimValueTypes.Integer));
+            }
         }
     }
 }
