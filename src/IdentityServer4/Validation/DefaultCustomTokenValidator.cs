eb3928c89921ea60961d2941cee0bb05fdfef04b
Dominick Baier <dbaier@leastprivilege.com>
2017-11-11 10:18:34 +0100

    remove usings

--- src/IdentityServer4/Validation/DefaultCustomTokenValidator.cs
+++ src/IdentityServer4/Validation/DefaultCustomTokenValidator.cs
@@ -2,14 +2,9 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
-using IdentityModel;
-using IdentityServer4.Extensions;
-using IdentityServer4.Models;
 using IdentityServer4.Services;
 using IdentityServer4.Stores;
 using Microsoft.Extensions.Logging;
-using System.Linq;
-using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace IdentityServer4.Validation
