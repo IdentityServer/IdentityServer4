ae3927888a37701323198c30431d332936de8e19
Dominick Baier <dbaier@leastprivilege.com>
2017-11-27 13:45:58 +0100

    Added diagnostics page

---
+++ src/Host/Quickstart/Diagnostics/DiagnosticsController.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityServer4.Quickstart.UI
+{
+    [SecurityHeaders]
+    [Authorize]
+    public class DiagnosticsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
