1b2716207155993d26a5c9669e838903cc46cff9
Dominick Baier <dbaier@leastprivilege.com>
2017-11-27 13:44:02 +0100

    add copyright

--- src/Host/Quickstart/Grants/GrantsViewModel.cs
+++ src/Host/Quickstart/Grants/GrantsViewModel.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using System;
 using System.Collections.Generic;
 
 namespace IdentityServer4.Quickstart.UI
