0f4311ed3806ebcd89d7f2d689bea8df773fdcea
kevinlo <kevinlo2@hotmail.com>
2017-12-20 12:01:04 -0800

    Set the LoggedOutViewModel.ClientName to ClientId if the ClientName is null or empty.

--- src/Host/Quickstart/Account/AccountService.cs
+++ src/Host/Quickstart/Account/AccountService.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
@@ -127,7 +127,7 @@ namespace IdentityServer4.Quickstart.UI
             {
                 AutomaticRedirectAfterSignOut = AccountOptions.AutomaticRedirectAfterSignOut,
                 PostLogoutRedirectUri = logout?.PostLogoutRedirectUri,
-                ClientName = logout?.ClientName,
+                ClientName = string.IsNullOrEmpty(logout?.ClientName) ? logout?.ClientId : logout?.ClientName,
                 SignOutIframeUrl = logout?.SignOutIFrameUrl,
                 LogoutId = logoutId
             };
