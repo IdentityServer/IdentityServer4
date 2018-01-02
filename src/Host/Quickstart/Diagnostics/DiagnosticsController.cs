daa3542c24bcf6564f68c8adda8343d1a33941eb
Dominick Baier <dbaier@leastprivilege.com>
2018-01-02 17:01:55 +0100

    decoded client list on diagnostics page

--- src/Host/Quickstart/Diagnostics/DiagnosticsController.cs
+++ src/Host/Quickstart/Diagnostics/DiagnosticsController.cs
@@ -1,8 +1,10 @@
-﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
 using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,15 +14,16 @@ namespace IdentityServer4.Quickstart.UI
     [Authorize]
     public class DiagnosticsController : Controller
     {
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
             var localAddresses = new string[] { "127.0.0.1", "::1", HttpContext.Connection.LocalIpAddress.ToString() };
-            if (localAddresses.Contains(HttpContext.Connection.RemoteIpAddress.ToString()))
+            if (!localAddresses.Contains(HttpContext.Connection.RemoteIpAddress.ToString()))
             {
-                return View();
+                return NotFound();
             }
 
-            return NotFound();
+            var model = new DiagnosticsViewModel(await HttpContext.AuthenticateAsync());
+            return View(model);
         }
     }
 }
