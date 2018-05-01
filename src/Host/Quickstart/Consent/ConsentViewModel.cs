cb5c4c00541ba1d60344ddc80114f81ff527c423
Linda Lawton <lilaw@eg.dk>
2018-05-01 13:44:27 +0200

    adds description

--- src/Host/Quickstart/Consent/ConsentViewModel.cs
+++ src/Host/Quickstart/Consent/ConsentViewModel.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
@@ -9,6 +9,7 @@ namespace IdentityServer4.Quickstart.UI
     public class ConsentViewModel : ConsentInputModel
     {
         public string ClientName { get; set; }
+        public string Description { get; set; }
         public string ClientUrl { get; set; }
         public string ClientLogoUrl { get; set; }
         public bool AllowRememberConsent { get; set; }
