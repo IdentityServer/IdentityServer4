75b1a660c3cab2580112e6e0288f3f6bed8189f9
Dominick Baier <dbaier@leastprivilege.com>
2018-08-01 13:54:02 +0200

    move is4.csproj to top-level src folder, move host

---
+++ host/Startup.cs
@@ -0,0 +1,160 @@
+// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using Host.Configuration;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using Microsoft.IdentityModel.Tokens;
+using IdentityServer4;
+using IdentityServer4.Quickstart.UI;
+using Microsoft.Extensions.Configuration;
+using IdentityServer4.Validation;
+
+namespace Host
+{
+    public class Startup
+    {
+        private readonly IConfiguration _config;
+
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public IServiceProvider ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc()
+                .SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_2_1);
+
+            services.Configure<IISOptions>(iis =>
+            {
+                iis.AuthenticationDisplayName = "Windows";
+                iis.AutomaticAuthentication = false;
+            });
+
+            services.AddIdentityServer(options =>
+                {
+                    options.Events.RaiseSuccessEvents = true;
+                    options.Events.RaiseFailureEvents = true;
+                    options.Events.RaiseErrorEvents = true;
+                    options.Events.RaiseInformationEvents = true;
+                })
+                .AddInMemoryClients(Clients.Get())
+                //.AddInMemoryClients(_config.GetSection("Clients"))
+                .AddInMemoryIdentityResources(Resources.GetIdentityResources())
+                .AddInMemoryApiResources(Resources.GetApiResources())
+                .AddDeveloperSigningCredential()
+                .AddExtensionGrantValidator<Extensions.ExtensionGrantValidator>()
+                .AddExtensionGrantValidator<Extensions.NoSubjectExtensionGrantValidator>()
+                .AddJwtBearerClientAuthentication()
+                .AddAppAuthRedirectUriValidator()
+                .AddClientConfigurationValidator<DefaultClientConfigurationValidator>()
+                .AddTestUsers(TestUsers.Users);
+
+            services.AddExternalIdentityProviders();
+
+            return services.BuildServiceProvider(validateScopes: true);
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseMiddleware<Logging.RequestLoggerMiddleware>();
+            app.UseDeveloperExceptionPage();
+
+            app.UseIdentityServer();
+
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+
+    public static class ServiceExtensions
+    {
+        public static IServiceCollection AddExternalIdentityProviders(this IServiceCollection services)
+        {
+            // configures the OpenIdConnect handlers to persist the state parameter into the server-side IDistributedCache.
+            services.AddOidcStateDataFormatterCache("aad", "demoidsrv");
+
+            services.AddAuthentication()
+                .AddGoogle("Google", options =>
+                {
+                    options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+
+                    options.ClientId = "708996912208-9m4dkjb5hscn7cjrn5u0r4tbgkbj1fko.apps.googleusercontent.com";
+                    options.ClientSecret = "wdfPY6t8H8cecgjlxud__4Gh";
+                })
+                .AddOpenIdConnect("demoidsrv", "IdentityServer", options =>
+                {
+                    options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+                    options.SignOutScheme = IdentityServerConstants.SignoutScheme;
+
+                    options.Authority = "https://demo.identityserver.io/";
+                    options.ClientId = "implicit";
+                    options.ResponseType = "id_token";
+                    options.SaveTokens = true;
+                    options.CallbackPath = "/signin-idsrv";
+                    options.SignedOutCallbackPath = "/signout-callback-idsrv";
+                    options.RemoteSignOutPath = "/signout-idsrv";
+
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        NameClaimType = "name",
+                        RoleClaimType = "role"
+                    };
+                })
+                .AddOpenIdConnect("aad", "Azure AD", options =>
+                {
+                    options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+                    options.SignOutScheme = IdentityServerConstants.SignoutScheme;
+
+                    options.Authority = "https://login.windows.net/4ca9cb4c-5e5f-4be9-b700-c532992a3705";
+                    options.ClientId = "96e3c53e-01cb-4244-b658-a42164cb67a9";
+                    options.ResponseType = "id_token";
+                    options.CallbackPath = "/signin-aad";
+                    options.SignedOutCallbackPath = "/signout-callback-aad";
+                    options.RemoteSignOutPath = "/signout-aad";
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        NameClaimType = "name",
+                        RoleClaimType = "role"
+                    };
+                })
+                .AddOpenIdConnect("adfs", "ADFS", options =>
+                {
+                    options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+                    options.SignOutScheme = IdentityServerConstants.SignoutScheme;
+
+                    options.Authority = "https://adfs.leastprivilege.vm/adfs";
+                    options.ClientId = "c0ea8d99-f1e7-43b0-a100-7dee3f2e5c3c";
+                    options.ResponseType = "id_token";
+
+                    options.CallbackPath = "/signin-adfs";
+                    options.SignedOutCallbackPath = "/signout-callback-adfs";
+                    options.RemoteSignOutPath = "/signout-adfs";
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        NameClaimType = "name",
+                        RoleClaimType = "role"
+                    };
+                })
+                .AddWsFederation("adfs-wsfed", "ADFS with WS-Fed", options =>
+                {
+                    options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+                    options.SignOutScheme = IdentityServerConstants.SignoutScheme;
+
+                    options.MetadataAddress = "https://adfs4.local/federationmetadata/2007-06/federationmetadata.xml";
+                    options.Wtrealm = "urn:test";
+
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        NameClaimType = "name",
+                        RoleClaimType = "role"
+                    };
+                });
+
+            return services;
+        }
+    }
+}
