afa42e4ddce4f5e6cbdae055ea60ecbe52b9a49a
Brock Allen <brockallen@gmail.com>
2018-08-02 14:24:37 -0400

    make EndSession public #2469

--- src/Models/Messages/EndSession.cs
+++ src/Models/Messages/EndSession.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 using System.Collections.Generic;
@@ -8,10 +8,21 @@ namespace IdentityServer4.Models
     /// <summary>
     /// Models the data necessary for end session to trigger single signout.
     /// </summary>
-    internal class EndSession
+    public class EndSession
     {
+        /// <summary>
+        ///  The SubjectId of the user.
+        /// </summary>
         public string SubjectId { get; set; }
+
+        /// <summary>
+        /// The session Id of the user's authentication session.
+        /// </summary>
         public string SessionId { get; set; }
+
+        /// <summary>
+        /// The list of client Ids that the user has authenticated to.
+        /// </summary>
         public IEnumerable<string> ClientIds { get; set; }
     }
 }
