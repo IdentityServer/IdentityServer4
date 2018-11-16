3105b32b4398d6e41fde6ea31bd8b137e59906c8
Dominick Baier <dbaier@leastprivilege.com>
2018-11-16 17:04:07 +0100

    update bash bootstrapper

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Define variables
 CAKE_VERSION=0.30.0
-DOTNET_VERSION=2.1.403
+DOTNET_VERSION=2.1.500
 
 SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 TOOLS_DIR=$SCRIPT_DIR/tools
