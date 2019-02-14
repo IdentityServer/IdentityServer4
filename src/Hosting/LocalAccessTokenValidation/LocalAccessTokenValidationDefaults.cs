fbe7156cd4775779565767d2084cf8a434b59a5b
Kahbazi <a.kahbazi@gmail.com>
2019-02-14 10:11:44 +0330

    Add LocalAccessTokenValidation authentication scheme

---
+++ src/Hosting/LocalAccessTokenValidation/LocalAccessTokenValidationDefaults.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IdentityServer4.Hosting.LocalAccessTokenValidation
+{
+    /// <summary>
+    /// Constants for IdentityServer authentication.
+    /// </summary>
+    public static class LocalAccessTokenValidationDefaults
+    {
+        /// <summary>
+        /// The authentication scheme
+        /// </summary>
+        public const string AuthenticationScheme = "LocalAccessTokenValidation";
+    }
+}
---
+++ src/Hosting/LocalAccessTokenValidation/LocalAccessTokenValidationExtensions.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Authentication;
+using System;
+
+namespace IdentityServer4.Hosting.LocalAccessTokenValidation
+{
+    /// <summary>
+    /// Extensions for registering the local access token authentication handler
+    /// </summary>
+    public static class LocalAccessTokenValidationExtensions
+    {
+        /// <summary>
+        /// Registers the IdentityServer authentication handler.
+        /// </summary>
+        /// <param name="builder">The builder.</param>
+        /// <returns></returns>
+        public static AuthenticationBuilder AddLocalAccessTokenValidation(this AuthenticationBuilder builder)
+            => builder.AddLocalAccessTokenValidation(LocalAccessTokenValidationDefaults.AuthenticationScheme, _ => { });
+
+        /// <summary>
+        /// Registers the IdentityServer authentication handler.
+        /// </summary>
+        /// <param name="builder">The builder.</param>
+        /// <param name="configureOptions">The configure options.</param>
+        /// <returns></returns>
+        public static AuthenticationBuilder AddLocalAccessTokenValidation(this AuthenticationBuilder builder, Action<LocalAccessTokenValidationOptions> configureOptions)
+            => builder.AddLocalAccessTokenValidation(LocalAccessTokenValidationDefaults.AuthenticationScheme, configureOptions);
+
+        /// <summary>
+        /// Registers the IdentityServer authentication handler.
+        /// </summary>
+        /// <param name="builder">The builder.</param>
+        /// <param name="authenticationScheme">The authentication scheme.</param>
+        /// <param name="configureOptions">The configure options.</param>
+        /// <returns></returns>
+        public static AuthenticationBuilder AddLocalAccessTokenValidation(this AuthenticationBuilder builder, string authenticationScheme, Action<LocalAccessTokenValidationOptions> configureOptions)
+            => builder.AddLocalAccessTokenValidation(authenticationScheme, displayName: null, configureOptions: configureOptions);
+
+        /// <summary>
+        /// Registers the IdentityServer authentication handler.
+        /// </summary>
+        /// <param name="builder">The builder.</param>
+        /// <param name="authenticationScheme">The authentication scheme.</param>
+        /// <param name="displayName">The display name of this scheme.</param>
+        /// <param name="configureOptions">The configure options.</param>
+        /// <returns></returns>
+        public static AuthenticationBuilder AddLocalAccessTokenValidation(this AuthenticationBuilder builder, string authenticationScheme, string displayName, Action<LocalAccessTokenValidationOptions> configureOptions)
+        {
+            return builder.AddScheme<LocalAccessTokenValidationOptions, LocalAccessTokenValidationHandler>(authenticationScheme, displayName, configureOptions);
+        }
+    }
+}
