de12e4028fa61c1a0440e3fc52a13fc9929aff60
Dominick Baier <dbaier@leastprivilege.com>
2019-02-17 10:53:20 +0100

    add copyright

--- src/Configuration/DependencyInjection/ConfigureInternalCookieOptions.cs
+++ src/Configuration/DependencyInjection/ConfigureInternalCookieOptions.cs
@@ -1,3 +1,7 @@
+// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
 using IdentityServer4.Extensions;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Http;
