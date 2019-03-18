58a4fcd1e87a598f190b8820c70b280a6cfb2941
Dominick Baier <dbaier@leastprivilege.com>
2019-03-18 13:50:20 -0700

    first stab at build all

---
+++ build.ps1
@@ -0,0 +1,64 @@
+New-Item -ItemType Directory -Force -Path ./nuget
+
+$cd = Get-Location
+
+# storage
+set-location ./src/Storage
+& ./build.ps1
+Set-Location $cd
+
+if ($LASTEXITCODE -ne 0)
+{
+    exit $LASTEXITCODE
+}
+
+Copy-Item -path .\src\Storage\artifacts\packages\*.nupkg -Destination .\nuget
+
+# EF storage
+set-location ./src/EntityFramework.Storage
+& ./build.ps1
+Set-Location $cd
+
+if ($LASTEXITCODE -ne 0)
+{
+    exit $LASTEXITCODE
+}
+
+Copy-Item -path .\src\EntityFramework.Storage\artifacts\packages\*.nupkg -Destination .\nuget
+
+# EF
+set-location ./src/EntityFramework
+& ./build.ps1
+Set-Location $cd
+
+if ($LASTEXITCODE -ne 0)
+{
+    exit $LASTEXITCODE
+}
+
+Copy-Item -path .\src\EntityFramework\artifacts\packages\*.nupkg -Destination .\nuget
+
+# core
+set-location ./src/IdentityServer4
+& ./build.ps1
+Set-Location $cd
+
+if ($LASTEXITCODE -ne 0)
+{
+    exit $LASTEXITCODE
+}
+
+Copy-Item -path .\src\IdentityServer4\artifacts\packages\*.nupkg -Destination .\nuget
+
+# aspid
+set-location ./src/AspNetIdentity
+& ./build.ps1
+Set-Location $cd
+
+if ($LASTEXITCODE -ne 0)
+{
+    exit $LASTEXITCODE
+}
+
+Copy-Item -path .\src\AspNetIdentity\artifacts\packages\*.nupkg -Destination .\nuget
+
