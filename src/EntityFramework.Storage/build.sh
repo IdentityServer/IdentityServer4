7897a874963fdbf55963fb5f0b7f6a0df8c207d1
Dominick Baier <dbaier@leastprivilege.com>
2019-03-21 11:25:18 -0700

    remove .net sdk install from bash

--- src/EntityFramework.Storage/build.sh
+++ src/EntityFramework.Storage/build.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 # Define variables
 CAKE_VERSION=0.30.0
-DOTNET_VERSION=2.2.104
 
 SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 TOOLS_DIR=$SCRIPT_DIR/tools
@@ -12,27 +11,6 @@ CAKE_PATH=$TOOLS_DIR/.store/cake.tool/$CAKE_VERSION
 if [ ! -d "$TOOLS_DIR" ]; then
   mkdir "$TOOLS_DIR"
 fi
-
-###########################################################################
-# INSTALL .NET CORE CLI
-###########################################################################
-
-export PATH="$SCRIPT_DIR/.dotnet":$PATH
-INSTALLED_DOTNET_VERSION=$( dotnet --version 2>&1 )
-
-if  [ "$INSTALLED_DOTNET_VERSION" != "$DOTNET_VERSION" ]; then
-    echo "Installing .NET CLI..."
-    if [ ! -d "$SCRIPT_DIR/.dotnet" ]; then
-      mkdir "$SCRIPT_DIR/.dotnet"
-    fi
-    curl -Lsfo "$SCRIPT_DIR/.dotnet/dotnet-install.sh" https://dot.net/v1/dotnet-install.sh
-    bash "$SCRIPT_DIR/.dotnet/dotnet-install.sh" --version $DOTNET_VERSION --install-dir .dotnet --no-path
-fi
-
-export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
-export DOTNET_CLI_TELEMETRY_OPTOUT=1
-export DOTNET_SYSTEM_NET_HTTP_USESOCKETSHTTPHANDLER=0
-
 
 ###########################################################################
 # INSTALL CAKE
