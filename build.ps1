f40ba1bd5aa48e77f0a17c1537150b6079f904a7
Dominick Baier <dbaier@leastprivilege.com>
2019-03-22 14:15:44 -0700

    fix 3.x build

--- build.ps1
+++ build.ps1
@@ -16,6 +16,19 @@ if ($LASTEXITCODE -ne 0)
 
 Copy-Item -path .\src\Storage\artifacts\packages\*.nupkg -Destination .\nuget
 
+# core
+"######### IdentityServer4 #########"
+set-location ./src/IdentityServer4
+& ./build.ps1 $args
+Set-Location $cd
+
+if ($LASTEXITCODE -ne 0)
+{
+    exit $LASTEXITCODE
+}
+
+Copy-Item -path .\src\IdentityServer4\artifacts\packages\*.nupkg -Destination .\nuget
+
 # EF storage
 "######### EntityFramework Storage #########"
 set-location ./src/EntityFramework.Storage
@@ -41,19 +54,6 @@ if ($LASTEXITCODE -ne 0)
 }
 
 Copy-Item -path .\src\EntityFramework\artifacts\packages\*.nupkg -Destination .\nuget
-
-# core
-"######### IdentityServer4 #########"
-set-location ./src/IdentityServer4
-& ./build.ps1 $args
-Set-Location $cd
-
-if ($LASTEXITCODE -ne 0)
-{
-    exit $LASTEXITCODE
-}
-
-Copy-Item -path .\src\IdentityServer4\artifacts\packages\*.nupkg -Destination .\nuget
 
 # aspid
 "######### ASP.NET Identity #########"
