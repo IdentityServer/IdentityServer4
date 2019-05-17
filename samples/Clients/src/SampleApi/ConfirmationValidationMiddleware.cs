8a79939b1fa60a8d8fba3b0e7a7f462fda15f80a
Dominick Baier <dbaier@leastprivilege.com>
2019-05-17 09:43:56 +0200

    updates

---
+++ samples/Clients/src/SampleApi/ConfirmationValidationMiddleware.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Threading.Tasks;
+
+namespace SampleApi
+{
+    // this middleware validate the cnf claim (if present) against the thumbprint of the X.509 client certificate for the current client
+    public class ConfirmationValidationMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ConfirmationValidationMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext ctx)
+        {
+            if (ctx.User.Identity.IsAuthenticated)
+            {
+                var cnfJson = ctx.User.FindFirst("cnf")?.Value;
+                if (!String.IsNullOrWhiteSpace(cnfJson))
+                {
+                    var certResult = await ctx.AuthenticateAsync("x509");
+                    if (!certResult.Succeeded)
+                    {
+                        await ctx.ChallengeAsync("x509");
+                        return;
+                    }
+
+                    var cert = ctx.Connection.ClientCertificate;
+                    if (cert == null)
+                    {
+                        await ctx.ChallengeAsync("x509");
+                        return;
+                    }
+
+                    var thumbprint = cert.Thumbprint;
+
+                    var cnf = JObject.Parse(cnfJson);
+                    var sha256 = cnf.Value<string>("x5t#S256");
+
+                    if (String.IsNullOrWhiteSpace(sha256) ||
+                        !thumbprint.Equals(sha256, StringComparison.OrdinalIgnoreCase))
+                    {
+                        await ctx.ChallengeAsync("token");
+                        return;
+                    }
+                }
+            }
+
+            await _next(ctx);
+        }
+    }
+}
--- samples/Clients/src/SampleApi/Startup.cs
+++ samples/Clients/src/SampleApi/Startup.cs
@@ -62,45 +62,8 @@ namespace SampleApi
             });
 
             app.UseAuthentication();
-
-            app.Use(async (ctx, next) =>
-            {
-                if (ctx.User.Identity.IsAuthenticated)
-                {
-                    var cnfJson = ctx.User.FindFirst("cnf")?.Value;
-                    if (!String.IsNullOrWhiteSpace(cnfJson))
-                    {
-                        var certResult = await ctx.AuthenticateAsync("x509");
-                        if (!certResult.Succeeded)
-                        {
-                            await ctx.ChallengeAsync("x509");
-                            return;
-                        }
-
-                        var cert = ctx.Connection.ClientCertificate;
-                        if (cert == null)
-                        {
-                            await ctx.ChallengeAsync("x509");
-                            return;
-                        }
-
-                        var thumbprint = cert.Thumbprint;
-
-                        var cnf = JObject.Parse(cnfJson);
-                        var sha256 = cnf.Value<string>("x5t#S256");
-
-                        if (String.IsNullOrWhiteSpace(sha256) ||
-                            !thumbprint.Equals(sha256, StringComparison.OrdinalIgnoreCase))
-                        {
-                            await ctx.ChallengeAsync("token");
-                            return;
-                        }
-                    }
-                }
-
-                await next();
-            });
-
+            app.UseMiddleware<ConfirmationValidationMiddleware>();
+            
             app.UseMvc();
         }
     }
