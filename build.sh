9893bbc6a51391e3c1c201523b2be49bafcf3bdd
Adam Ralph <adam@adamralph.com>
2019-07-28 17:16:48 +0300

    test MinVer in isolation in CI

--- build.sh
+++ build.sh
@@ -1,56 +1,5 @@
 mkdir nuget
-
 cd ./src/Storage
-./build.sh
-
-if [ $? -ne 0 ]; then
-    echo "An error occured."
-    exit 1
-fi
-
-cd ../..
-cp ./src/Storage/artifacts/* ./nuget
-
-cd ./src/IdentityServer4
-./build.sh
-
-if [ $? -ne 0 ]; then
-    echo "An error occured."
-    exit 1
-fi
-
-cd ../..
-cp ./src/IdentityServer4/artifacts/* ./nuget
-
-cd ./src/EntityFramework.Storage
-./build.sh
-
-if [ $? -ne 0 ]; then
-    echo "An error occured."
-    exit 1
-fi
-
-cd ../..
-cp ./src/EntityFramework.Storage/artifacts/* ./nuget
-
-cd ./src/EntityFramework
-./build.sh
-
-if [ $? -ne 0 ]; then
-    echo "An error occured."
-    exit 1
-fi
-
-cd ../..
-cp ./src/EntityFramework/artifacts/* ./nuget
-
-cd ./src/AspNetIdentity
-./build.sh
-
-if [ $? -ne 0 ]; then
-    echo "An error occured."
-    exit 1
-fi
-
-cd ../..
-cp ./src/AspNetIdentity/artifacts/* ./nuget
+dotnet --info
+dotnet restore
+dotnet "/home/vsts/.nuget/packages/minver/1.1.0/build/../minver/MinVer.dll"
