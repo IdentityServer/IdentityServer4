d8b360cbad86d63e2d629420f880dd3ddd5a8c45
Dominick Baier <dbaier@leastprivilege.com>
2019-07-31 09:16:57 +0200

    allow specifying signing algorithm on all helper methods

---
+++ src/IdentityServer4/src/Configuration/CryptoHelper.cs
@@ -0,0 +1,62 @@
+﻿using IdentityModel;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System.Reflection;
+using System.Security.Cryptography;
+
+namespace IdentityServer4.Configuration
+{
+    /// <summary>
+    /// Crypto helper
+    /// </summary>
+    public static class CryptoHelper
+    {
+        /// <summary>
+        /// Creates a new RSA security key.
+        /// </summary>
+        /// <returns></returns>
+        public static RsaSecurityKey CreateRsaSecurityKey(int keySize = 2048)
+        {
+            return new RsaSecurityKey(RSA.Create(keySize))
+            {
+                KeyId = CryptoRandom.CreateUniqueId(16)
+            };
+        }
+
+        /// <summary>
+        /// Creates an RSA security key.
+        /// </summary>
+        /// <param name="parameters">The parameters.</param>
+        /// <param name="id">The identifier.</param>
+        /// <returns></returns>
+        public static RsaSecurityKey CreateRsaSecurityKey(RSAParameters parameters, string id)
+        {
+            var key = new RsaSecurityKey(parameters)
+            {
+                KeyId = id
+            };
+
+            return key;
+        }
+
+        // used for serialization to temporary RSA key
+        internal class TemporaryRsaKey
+        {
+            public string KeyId { get; set; }
+            public RSAParameters Parameters { get; set; }
+        }
+
+        internal class RsaKeyContractResolver : DefaultContractResolver
+        {
+            protected override JsonProperty CreateProperty(MemberInfo member, MemberSerialization memberSerialization)
+            {
+                var property = base.CreateProperty(member, memberSerialization);
+
+                property.Ignored = false;
+
+                return property;
+            }
+        }
+    }
+}
