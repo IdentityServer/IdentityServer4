e454911952ab4f83e35065fef2babff3ee6977df
Scott Brady <scott@scottbrady91.com>
2019-10-13 12:13:17 +0100

    Added failing test for flow query

--- src/EntityFramework.Storage/test/IntegrationTests/Stores/ClientStoreTests.cs
+++ src/EntityFramework.Storage/test/IntegrationTests/Stores/ClientStoreTests.cs
@@ -3,6 +3,7 @@
 
 
 using System.Linq;
+using System.Threading.Tasks;
 using FluentAssertions;
 using IdentityServer4.EntityFramework.DbContexts;
 using IdentityServer4.EntityFramework.Mappers;
@@ -11,6 +12,7 @@ using IdentityServer4.EntityFramework.Stores;
 using IdentityServer4.Models;
 using Microsoft.EntityFrameworkCore;
 using Xunit;
+using Xunit.Sdk;
 
 namespace IdentityServer4.EntityFramework.IntegrationTests.Stores
 {
@@ -28,7 +30,7 @@ namespace IdentityServer4.EntityFramework.IntegrationTests.Stores
         }
 
         [Theory, MemberData(nameof(TestDatabaseProviders))]
-        public void FindClientByIdAsync_WhenClientExists_ExpectClientRetured(DbContextOptions<ConfigurationDbContext> options)
+        public async Task FindClientByIdAsync_WhenClientExists_ExpectClientRetured(DbContextOptions<ConfigurationDbContext> options)
         {
             var testClient = new Client
             {
@@ -46,14 +48,14 @@ namespace IdentityServer4.EntityFramework.IntegrationTests.Stores
             using (var context = new ConfigurationDbContext(options, StoreOptions))
             {
                 var store = new ClientStore(context, FakeLogger<ClientStore>.Create());
-                client = store.FindClientByIdAsync(testClient.ClientId).Result;
+                client = await store.FindClientByIdAsync(testClient.ClientId);
             }
 
             Assert.NotNull(client);
         }
 
         [Theory, MemberData(nameof(TestDatabaseProviders))]
-        public void FindClientByIdAsync_WhenClientExists_ExpectClientPropertiesRetured(DbContextOptions<ConfigurationDbContext> options)
+        public async Task FindClientByIdAsync_WhenClientExists_ExpectClientPropertiesRetured(DbContextOptions<ConfigurationDbContext> options)
         {
             var testClient = new Client
             {
@@ -76,7 +78,7 @@ namespace IdentityServer4.EntityFramework.IntegrationTests.Stores
             using (var context = new ConfigurationDbContext(options, StoreOptions))
             {
                 var store = new ClientStore(context, FakeLogger<ClientStore>.Create());
-                client = store.FindClientByIdAsync(testClient.ClientId).Result;
+                client = await store.FindClientByIdAsync(testClient.ClientId);
             }
 
             client.Properties.Should().NotBeNull();
@@ -85,6 +87,64 @@ namespace IdentityServer4.EntityFramework.IntegrationTests.Stores
             client.Properties.ContainsKey("foo2").Should().BeTrue();
             client.Properties["foo1"].Should().Be("bar1");
             client.Properties["foo2"].Should().Be("bar2");
+        }
+
+        [Theory, MemberData(nameof(TestDatabaseProviders))]
+        public async Task FindClientByIdAsync_WhenClientsExistWithManyRelations_ExpectClientReturnedInUnderFiveSeconds(DbContextOptions<ConfigurationDbContext> options)
+        {
+            var testClient = new Client
+            {
+                ClientId = "test_client_with_uris",
+                ClientName = "Test client with URIs",
+                AllowedScopes = {"openid", "profile", "api1"},
+                AllowedGrantTypes = GrantTypes.CodeAndClientCredentials
+            };
+
+            for (int i = 0; i < 50; i++)
+            {
+                testClient.RedirectUris.Add($"https://localhost/{i}");
+                testClient.PostLogoutRedirectUris.Add($"https://localhost/{i}");
+                testClient.AllowedCorsOrigins.Add($"https://localhost:{i}");
+            }
+
+            using (var context = new ConfigurationDbContext(options, StoreOptions))
+            {
+                context.Clients.Add(testClient.ToEntity());
+
+                for (int i = 0; i < 50; i++)
+                {
+                    context.Clients.Add(new Client
+                    {
+                        ClientId = testClient.ClientId + i,
+                        ClientName = testClient.ClientName,
+                        AllowedScopes = testClient.AllowedScopes,
+                        AllowedGrantTypes = testClient.AllowedGrantTypes,
+                        RedirectUris = testClient.RedirectUris,
+                        PostLogoutRedirectUris = testClient.PostLogoutRedirectUris,
+                        AllowedCorsOrigins = testClient.AllowedCorsOrigins,
+                    }.ToEntity());
+                }
+
+                context.SaveChanges();
+            }
+            
+            using (var context = new ConfigurationDbContext(options, StoreOptions))
+            {
+                var store = new ClientStore(context, FakeLogger<ClientStore>.Create());
+
+                const int timeout = 5000;
+                var task = Task.Run(() => store.FindClientByIdAsync(testClient.ClientId));
+
+                if (await Task.WhenAny(task, Task.Delay(timeout)) == task)
+                {
+                    var client = task.Result;
+                    client.Should().BeEquivalentTo(testClient);
+                }
+                else
+                {
+                    throw new TestTimeoutException(timeout);
+                }
+            }
         }
     }
 }
