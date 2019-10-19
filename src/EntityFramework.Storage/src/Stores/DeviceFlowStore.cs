719813ed5c04d529dfe7f18ad3d2c9dbfe8aeb22
AliBazzi <1574502+AliBazzi@users.noreply.github.com>
2019-10-20 01:00:46 +0800

    Revert "simplify StoreDeviceAuthorizationAsync."
    This reverts commit b7cede851db4bb3ffc6b331fc75edacca21e69eb.

--- src/EntityFramework.Storage/src/Stores/DeviceFlowStore.cs
+++ src/EntityFramework.Storage/src/Stores/DeviceFlowStore.cs
@@ -59,11 +59,11 @@ namespace IdentityServer4.EntityFramework.Stores
         /// <param name="userCode">The user code.</param>
         /// <param name="data">The data.</param>
         /// <returns></returns>
-        public virtual Task StoreDeviceAuthorizationAsync(string deviceCode, string userCode, DeviceCode data)
+        public virtual async Task StoreDeviceAuthorizationAsync(string deviceCode, string userCode, DeviceCode data)
         {
             Context.DeviceFlowCodes.Add(ToEntity(data, deviceCode, userCode));
 
-            return Context.SaveChangesAsync();
+            await Context.SaveChangesAsync();
         }
 
         /// <summary>
