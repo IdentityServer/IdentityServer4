07dffbad52fdfcd470905c5143db41662a361f4e
Dominick Baier <dbaier@leastprivilege.com>
2019-10-22 11:42:56 +0200

    Update Quickstarts 1-3 for ASP.NET Core 3 (#3746)
    * update qs1 code
    
    * update qs1
    
    * update qs1 code
    
    * update qs1 code
    
    * update qs1 text
    
    * remove password grant type QS
    
    * update qs2 code
    
    * update qs2 code
    
    * update qs2 text
    
    * qs2 updates
    
    * update qs2 code to external authN
    
    * update qs2 text for external authN
    
    * remove file logger
    
    * switch statement hipster treatment
    
    * add note about versions to QS overview
    
    * add QS3 text
    
    * add code for QS3
    
    * add qs3 to index
    
    * update QS3 test
    
    * minor cleanup

--- samples/Quickstarts/1_ClientCredentials/src/IdentityServer/Startup.cs
+++ samples/Quickstarts/1_ClientCredentials/src/IdentityServer/Startup.cs
@@ -2,56 +2,41 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
-using System;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace IdentityServer
 {
     public class Startup
     {
-        public IHostingEnvironment Environment { get; }
-
-        public Startup(IHostingEnvironment environment)
-        {
-            Environment = environment;
-        }
-
         public void ConfigureServices(IServiceCollection services)
         {
-            // uncomment, if you wan to add an MVC-based UI
-            //services.AddMvc().SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_2_1);
+            // uncomment, if you want to add an MVC-based UI
+            //services.AddControllersWithViews();
 
             var builder = services.AddIdentityServer()
-                .AddInMemoryIdentityResources(Config.GetIdentityResources())
-                .AddInMemoryApiResources(Config.GetApis())
-                .AddInMemoryClients(Config.GetClients());
+                .AddInMemoryApiResources(Config.Apis)
+                .AddInMemoryClients(Config.Clients);
 
-            if (Environment.IsDevelopment())
-            {
-                builder.AddDeveloperSigningCredential();
-            }
-            else
-            {
-                throw new Exception("need to configure key material");
-            }
+            builder.AddDeveloperSigningCredential();
         }
 
         public void Configure(IApplicationBuilder app)
         {
-            if (Environment.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            app.UseDeveloperExceptionPage();
 
-            // uncomment if you want to support static files
+            // uncomment if you want to add MVC
             //app.UseStaticFiles();
+            //app.UseRouting();
 
             app.UseIdentityServer();
 
-            // uncomment, if you wan to add an MVC-based UI
-            //app.UseMvcWithDefaultRoute();
+            // uncomment, if you want to add MVC-based
+            //app.UseAuthorization();
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapDefaultControllerRoute();
+            //});
         }
     }
 }
