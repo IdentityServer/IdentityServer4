6bd9daa2768480b02f00c6250d5c91c7d6b2e47a
Dominick Baier <dbaier@leastprivilege.com>
2019-10-23 15:27:55 +0200

    Change HttpRequest/Response extension method namespace (#3704)
    * Change namespace of extension methods for ASP.NET Core owned types
    
    * more usings

--- src/IdentityServer4/src/Endpoints/Results/CheckSessionResult.cs
+++ src/IdentityServer4/src/Endpoints/Results/CheckSessionResult.cs
@@ -7,6 +7,7 @@ using IdentityServer4.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using IdentityServer4.Configuration;
+using IdentityServer4.Extensions;
 
 namespace IdentityServer4.Endpoints.Results
 {
--- src/IdentityServer4/test/IdentityServer.UnitTests/Extensions/HttpRequestExtensionsTests.cs
+++ src/IdentityServer4/test/IdentityServer.UnitTests/Extensions/HttpRequestExtensionsTests.cs
@@ -3,6 +3,7 @@
 
 
 using FluentAssertions;
+using IdentityServer4.Extensions;
 using Microsoft.AspNetCore.Http;
 using Xunit;
 
