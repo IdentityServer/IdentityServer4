945ead68dc393b360e2d84cc06b9ff5ead44c0dd
stermic2 <46682781+stermic2@users.noreply.github.com>
2019-11-11 16:06:35 -0700

    Unnescessary pages deleted

--- samples/Clients/src/BlazorAuth/BlazorAuth/Pages/Login.cshtml.cs
+++ samples/Clients/src/BlazorAuth/BlazorAuth/Pages/Login.cshtml.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
--- samples/Clients/src/BlazorAuth/BlazorAuth/Pages/Logout.cshtml.cs
+++ samples/Clients/src/BlazorAuth/BlazorAuth/Pages/Logout.cshtml.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Components;
 
 namespace OktaBlazorAspNetCoreServerSide.Pages
 {
