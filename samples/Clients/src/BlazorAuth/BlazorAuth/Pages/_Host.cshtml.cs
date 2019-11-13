b7692665f21ff2a1d0457f70c02ea2a539beaae3
stermic2 <46682781+stermic2@users.noreply.github.com>
2019-11-13 14:47:50 -0700

    Minimal Implementation

---
+++ samples/Clients/src/BlazorAuth/BlazorAuth/Pages/_Host.cshtml.cs
@@ -0,0 +1,32 @@
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BlazorAuth.Pages
+{
+    public class HostModel : PageModel
+    {
+        [BindProperty(SupportsGet = true)] public string RedirectUri { get; set; }
+        [BindProperty(SupportsGet = true)] public bool LogIn { get; set; } = false;
+        [BindProperty(SupportsGet = true)] public bool LogOut { get; set; } = false;
+
+        public async Task OnGetAsync()
+        {
+            if (LogIn == true)
+            {
+                var prop = new AuthenticationProperties {RedirectUri = RedirectUri};
+                string auth = HttpContext.Request.Headers["Authorization"];
+                await HttpContext.ChallengeAsync(prop);
+            }
+            if (LogOut == true)
+            {
+                var prop = new AuthenticationProperties {RedirectUri = RedirectUri};
+                await HttpContext.SignOutAsync("Cookies");
+                await HttpContext.SignOutAsync(OpenIdConnectDefaults.AuthenticationScheme, prop);
+            }
+        }
+    }
+}
--- samples/Clients/src/BlazorAuth/BlazorAuth/Startup.cs
+++ samples/Clients/src/BlazorAuth/BlazorAuth/Startup.cs
@@ -25,7 +25,6 @@ namespace BlazorAuth
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddAuthorizationCore();
             services.AddAuthentication(options =>
             {
                 options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
@@ -61,33 +60,13 @@ namespace BlazorAuth
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
             app.UseRouting();
 
             app.UseAuthentication();
-            app.UseAuthorization();
-
-
+            
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllers();
                 endpoints.MapRazorPages();
-                endpoints.MapBlazorHub();
-                endpoints.MapFallbackToPage("/_Host");
             });
         }
     }
