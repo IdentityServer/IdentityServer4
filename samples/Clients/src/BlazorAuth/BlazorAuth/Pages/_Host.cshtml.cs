62b9ecb13ac337b5c7d8083da1476c95c0302d20
stermic2 <46682781+stermic2@users.noreply.github.com>
2019-11-18 11:14:55 -0700

    Authorization properties and user properties shown.

--- samples/Clients/src/BlazorAuth/BlazorAuth/Pages/_Host.cshtml.cs
+++ samples/Clients/src/BlazorAuth/BlazorAuth/Pages/_Host.cshtml.cs
@@ -12,20 +12,22 @@ namespace BlazorAuth.Pages
         [BindProperty(SupportsGet = true)] public bool LogIn { get; set; } = false;
         [BindProperty(SupportsGet = true)] public bool LogOut { get; set; } = false;
 
+        //M.S. For Development Only: Authentication Properties made public for demonstration only!
+        public static AuthenticationProperties properties;
+
         public async Task OnGetAsync()
         {
             if (LogIn)
             {
-                var prop = new AuthenticationProperties {RedirectUri = RedirectUri};
-                string auth = HttpContext.Request.Headers["Authorization"];
-                await HttpContext.ChallengeAsync(prop);
+                properties = new AuthenticationProperties {RedirectUri = RedirectUri};
+                await HttpContext.ChallengeAsync(properties);
             }
 
             if (LogOut)
             {
-                var prop = new AuthenticationProperties {RedirectUri = RedirectUri};
+                properties = new AuthenticationProperties {RedirectUri = RedirectUri};
                 await HttpContext.SignOutAsync("Cookies");
-                await HttpContext.SignOutAsync(OpenIdConnectDefaults.AuthenticationScheme, prop);
+                await HttpContext.SignOutAsync(OpenIdConnectDefaults.AuthenticationScheme, properties);
             }
         }
     }
