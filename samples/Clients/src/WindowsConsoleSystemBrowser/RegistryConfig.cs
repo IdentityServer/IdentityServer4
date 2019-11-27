2be5d0bb83408a0f69f18cad761cc1ae1ff7cb2a
Dominick Baier <dbaier@leastprivilege.com>
2019-11-27 17:40:01 +0100

    added extension methods that clears location header

---
+++ samples/Clients/src/WindowsConsoleSystemBrowser/RegistryConfig.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Reflection;
+using Microsoft.Win32;
+
+namespace WindowsConsoleSystemBrowser
+{
+    class RegistryConfig
+    {
+        public RegistryConfig(string uriScheme)
+        {
+            CustomUriScheme = uriScheme;
+        }
+
+        public void Configure()
+        {
+            if (NeedToAddKeys()) AddRegKeys();
+        }
+
+        private string CustomUriScheme { get; }
+
+        string CustomUriSchemeKeyPath => RootKeyPath + @"\" + CustomUriScheme;
+        string CustomUriSchemeKeyValueValue => "URL:" + CustomUriScheme;
+        string CommandKeyPath => CustomUriSchemeKeyPath + @"\shell\open\command";
+
+        const string RootKeyPath = @"Software\Classes";
+
+        const string CustomUriSchemeKeyValueName = "";
+
+        const string ShellKeyName = "shell";
+        const string OpenKeyName = "open";
+        const string CommandKeyName = "command";
+
+        const string CommandKeyValueName = "";
+        const string CommandKeyValueFormat = "\"{0}\" \"%1\"";
+        static string CommandKeyValueValue => String.Format(CommandKeyValueFormat, Assembly.GetExecutingAssembly().Location);
+
+        const string UrlProtocolValueName = "URL Protocol";
+        const string UrlProtocolValueValue = "";
+
+        bool NeedToAddKeys()
+        {
+            var addKeys = false;
+
+            using (var commandKey = Registry.CurrentUser.OpenSubKey(CommandKeyPath))
+            {
+                var commandValue = commandKey?.GetValue(CommandKeyValueName);
+                addKeys |= !CommandKeyValueValue.Equals(commandValue);
+            }
+
+            using (var customUriSchemeKey = Registry.CurrentUser.OpenSubKey(CustomUriSchemeKeyPath))
+            {
+                var uriValue = customUriSchemeKey?.GetValue(CustomUriSchemeKeyValueName);
+                var protocolValue = customUriSchemeKey?.GetValue(UrlProtocolValueName);
+
+                addKeys |= !CustomUriSchemeKeyValueValue.Equals(uriValue);
+                addKeys |= !UrlProtocolValueValue.Equals(protocolValue);
+            }
+
+            return addKeys;
+        }
+
+        void AddRegKeys()
+        {
+            using (var classesKey = Registry.CurrentUser.OpenSubKey(RootKeyPath, true))
+            {
+                using (var root = classesKey.OpenSubKey(CustomUriScheme, true) ??
+                    classesKey.CreateSubKey(CustomUriScheme, true))
+                {
+                    root.SetValue(CustomUriSchemeKeyValueName, CustomUriSchemeKeyValueValue);
+                    root.SetValue(UrlProtocolValueName, UrlProtocolValueValue);
+
+                    using (var shell = root.OpenSubKey(ShellKeyName, true) ??
+                            root.CreateSubKey(ShellKeyName, true))
+                    {
+                        using (var open = shell.OpenSubKey(OpenKeyName, true) ??
+                                shell.CreateSubKey(OpenKeyName, true))
+                        {
+                            using (var command = open.OpenSubKey(CommandKeyName, true) ??
+                                    open.CreateSubKey(CommandKeyName, true))
+                            {
+                                command.SetValue(CommandKeyValueName, CommandKeyValueValue);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
