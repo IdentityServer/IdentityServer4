16604dadf0c63d93612abe114d18edd2f705b0c5
Dominick Baier <dbaier@leastprivilege.com>
2019-11-28 07:58:07 +0100

    reset status code

--- src/IdentityServer4/host/Quickstart/Extensions.cs
+++ src/IdentityServer4/host/Quickstart/Extensions.cs
@@ -25,7 +25,9 @@ namespace IdentityServer4.Quickstart.UI
 
         public static IActionResult LoadingPage(this Controller controller, string viewName, string redirectUri)
         {
+            controller.HttpContext.Response.StatusCode = 200;
             controller.HttpContext.Response.Headers["Location"] = "";
+            
             return controller.View(viewName, new RedirectViewModel { RedirectUrl = redirectUri });
         }
     }
