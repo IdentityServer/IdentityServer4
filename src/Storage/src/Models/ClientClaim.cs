3c6272e9653886834015cad76b79e66b3941a9f5
Dominick Baier <dbaier@leastprivilege.com>
2019-12-09 18:38:53 +0100

    add equality for ClientClaim

--- src/Storage/src/Models/ClientClaim.cs
+++ src/Storage/src/Models/ClientClaim.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
+using System;
 using System.Security.Claims;
 
 namespace IdentityServer4.Models
@@ -37,6 +38,34 @@ namespace IdentityServer4.Models
             Type = type;
             Value = value;
             ValueType = valueType;
+        }
+
+        /// <inheritdoc/>
+        public override int GetHashCode()
+        {
+            unchecked 
+            {
+                int hash = 17;
+
+                hash = hash * 23 + Value.GetHashCode();
+                hash = hash * 23 + Type.GetHashCode();
+                hash = hash * 23 + ValueType.GetHashCode();
+                return hash;
+            }
+        }
+
+        /// <inheritdoc/>
+        public override bool Equals(object obj)
+        {
+            if (obj is null) return false;
+            if (obj is ClientClaim c)
+            {
+                return (string.Equals(Type, c.Type, StringComparison.Ordinal) &&
+                        string.Equals(Value, c.Value, StringComparison.Ordinal) &&
+                        string.Equals(ValueType, c.ValueType, StringComparison.Ordinal));
+            }
+
+            return false;
         }
     }
 }
