417737a9daf12b9514c9707e8e2b911eb39a6556
Brock Allen <brockallen@gmail.com>
2019-12-10 15:11:09 -0500

    update test db names for 3.1

--- src/EntityFramework.Storage/test/IntegrationTests/DatabaseProviderBuilder.cs
+++ src/EntityFramework.Storage/test/IntegrationTests/DatabaseProviderBuilder.cs
@@ -21,7 +21,7 @@ namespace IdentityServer4.EntityFramework.IntegrationTests
         public static DbContextOptions<T> BuildSqlite<T>(string name) where T : DbContext
         {
             var builder = new DbContextOptionsBuilder<T>();
-            builder.UseSqlite($"Filename=./Test.IdentityServer4.EntityFramework-2.0.0.{name}.db");
+            builder.UseSqlite($"Filename=./Test.IdentityServer4.EntityFramework-3.1.0.{name}.db");
             return builder.Options;
         }
 
@@ -29,29 +29,15 @@ namespace IdentityServer4.EntityFramework.IntegrationTests
         {
             var builder = new DbContextOptionsBuilder<T>();
             builder.UseSqlServer(
-                $@"Data Source=(LocalDb)\MSSQLLocalDB;database=Test.IdentityServer4.EntityFramework-2.0.0.{name};trusted_connection=yes;");
+                $@"Data Source=(LocalDb)\MSSQLLocalDB;database=Test.IdentityServer4.EntityFramework-3.1.0.{name};trusted_connection=yes;");
             return builder.Options;
         }
 
         public static DbContextOptions<T> BuildAppVeyorSqlServer2016<T>(string name) where T : DbContext
         {
             var builder = new DbContextOptionsBuilder<T>();
-            builder.UseSqlServer($@"Server=(local)\SQL2016;Database=Test.IdentityServer4.EntityFramework-2.0.0.{name};User ID=sa;Password=Password12!");
+            builder.UseSqlServer($@"Server=(local)\SQL2016;Database=Test.IdentityServer4.EntityFramework-3.1.0.{name};User ID=sa;Password=Password12!");
             return builder.Options;
         }
-
-        //public static DbContextOptions<T> BuildAppVeyorMySql<T>(string name) where T : DbContext
-        //{
-        //    var builder = new DbContextOptionsBuilder<T>();
-        //    builder.UseMySql($@"server=localhost;database=Test.IdentityServer4-2.0.0.{name};userid=root;pwd=Password12!;port=3306;persistsecurityinfo=True;");
-        //    return builder.Options;
-        //}
-
-        //public static DbContextOptions<T> BuildAppVeyorPostgreSql<T>(string name) where T : DbContext
-        //{
-        //    var builder = new DbContextOptionsBuilder<T>();
-        //    builder.UseNpgsql($@"Host=localhost;Database=Test.IdentityServer4-2.0.0.{name};Username=postgres;Password=Password12!;Port=5432;");
-        //    return builder.Options;
-        //}
     }
 }
