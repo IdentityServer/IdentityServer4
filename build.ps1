321fe846f33fe1975eee09bd1203b39d91f01c17
Adam Ralph <adam@adamralph.com>
2020-04-21 14:21:06 +0200

    update to new build script (#4303)
    * update to new build script
    
    * fixup! update to new build script

--- build.ps1
+++ build.ps1
@@ -5,21 +5,21 @@ New-Item -ItemType Directory -Force -Path ./nuget
 dotnet tool restore
 
 pushd ./src/Storage
-./build.ps1 $args
+Invoke-Expression "./build.ps1 $args"
 popd
 
 pushd ./src/IdentityServer4
-./build.ps1 $args
+Invoke-Expression "./build.ps1 $args"
 popd
 
 pushd ./src/EntityFramework.Storage
-./build.ps1 $args
+Invoke-Expression "./build.ps1 $args"
 popd
 
 pushd ./src/EntityFramework
-./build.ps1 $args
+Invoke-Expression "./build.ps1 $args"
 popd
 
 pushd ./src/AspNetIdentity
-./build.ps1 $args
+Invoke-Expression "./build.ps1 $args"
 popd
