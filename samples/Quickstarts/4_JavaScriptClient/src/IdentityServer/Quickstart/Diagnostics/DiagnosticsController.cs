404b923d3022944cb97e89069ae9cf8ec50c3d6b
Victorio Berra <toryberra@gmail.com>
2020-05-01 09:47:56 -0500

    feat(DiagnosticsController): Use `[Host("localhost:*")]` over manual RemoteIpAddress check.
    See https://docs.microsoft.com/en-us/aspnet/core/fundamentals/routing?view=aspnetcore-3.1#host-matching-in-routes-with-requirehost for more details.
    
    ref #3372

--- samples/Quickstarts/4_JavaScriptClient/src/IdentityServer/Quickstart/Diagnostics/DiagnosticsController.cs
+++ samples/Quickstarts/4_JavaScriptClient/src/IdentityServer/Quickstart/Diagnostics/DiagnosticsController.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
 
 namespace IdentityServer
 {
@@ -14,14 +15,9 @@ namespace IdentityServer
     [Authorize]
     public class DiagnosticsController : Controller
     {
+        [Host("localhost:*")]
         public async Task<IActionResult> Index()
         {
-            var localAddresses = new string[] { "127.0.0.1", "::1", HttpContext.Connection.LocalIpAddress.ToString() };
-            if (!localAddresses.Contains(HttpContext.Connection.RemoteIpAddress.ToString()))
-            {
-                return NotFound();
-            }
-
             var model = new DiagnosticsViewModel(await HttpContext.AuthenticateAsync());
             return View(model);
         }
