c4540eb6bbd0f138a0b2d503cdf3721d91d609d1
David Barrows <davidbarrows@users.noreply.github.com>
2020-06-11 14:50:27 +0100

    update quickstart 5 port settings, and one I forgot previously, for quickstart 4

--- samples/Quickstarts/5_EntityFramework/src/MvcClient/Controllers/HomeController.cs
+++ samples/Quickstarts/5_EntityFramework/src/MvcClient/Controllers/HomeController.cs
@@ -30,7 +30,7 @@ namespace MvcClient.Controllers
 
             var client = new HttpClient();
             client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
-            var content = await client.GetStringAsync("http://localhost:5001/identity");
+            var content = await client.GetStringAsync("https://localhost:6001/identity");
 
             ViewBag.Json = JArray.Parse(content).ToString();
             return View("json");
