4b5e2c6cee41ac8ddcea3ecd1fadff1d58a0056f
David Barrows <davidbarrows@users.noreply.github.com>
2020-06-13 10:23:05 +0100

    update Quickstart 3 to use https, and new port(s) (#4485)

--- samples/Quickstarts/3_AspNetCoreAndApis/src/Client/Program.cs
+++ samples/Quickstarts/3_AspNetCoreAndApis/src/Client/Program.cs
@@ -16,7 +16,7 @@ namespace Client
             // discover endpoints from metadata
             var client = new HttpClient();
 
-            var disco = await client.GetDiscoveryDocumentAsync("http://localhost:5000");
+            var disco = await client.GetDiscoveryDocumentAsync("https://localhost:5001");
             if (disco.IsError)
             {
                 Console.WriteLine(disco.Error);
@@ -46,7 +46,7 @@ namespace Client
             var apiClient = new HttpClient();
             apiClient.SetBearerToken(tokenResponse.AccessToken);
 
-            var response = await apiClient.GetAsync("http://localhost:5001/identity");
+            var response = await apiClient.GetAsync("https://localhost:6001/identity");
             if (!response.IsSuccessStatusCode)
             {
                 Console.WriteLine(response.StatusCode);
