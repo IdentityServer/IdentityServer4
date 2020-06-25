6824fb5c08fc114db3630f730922aabcef962275
Brock Allen <brockallen@gmail.com>
2020-06-25 15:17:35 -0400

    update QS4

--- samples/Quickstarts/4_JavaScriptClient/src/JavaScriptClient/wwwroot/app.js
+++ samples/Quickstarts/4_JavaScriptClient/src/JavaScriptClient/wwwroot/app.js
@@ -1,4 +1,4 @@
-﻿/// <reference path="oidc-client.js" />
+/// <reference path="oidc-client.js" />
 
 function log() {
     document.getElementById('results').innerText = '';
@@ -21,10 +21,10 @@ document.getElementById("logout").addEventListener("click", logout, false);
 var config = {
     authority: "https://localhost:5001",
     client_id: "js",
-    redirect_uri: "http://localhost:5003/callback.html",
+    redirect_uri: "https://localhost:5003/callback.html",
     response_type: "code",
     scope:"openid profile api1",
-    post_logout_redirect_uri : "http://localhost:5003/index.html",
+    post_logout_redirect_uri : "https://localhost:5003/index.html",
 };
 var mgr = new Oidc.UserManager(config);
 
