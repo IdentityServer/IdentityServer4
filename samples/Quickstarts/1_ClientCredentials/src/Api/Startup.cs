45c3761ea4855b70b9bee5a7c90d8b7e548a1709
Dominick Baier <dbaier@leastprivilege.com>
2020-06-25 13:58:06 +0200

    update qs1 text

--- samples/Quickstarts/1_ClientCredentials/src/Api/Startup.cs
+++ samples/Quickstarts/1_ClientCredentials/src/Api/Startup.cs
@@ -13,6 +13,7 @@ namespace Api
         {
             services.AddControllers();
 
+            // accepts any access token issued by identity server
             services.AddAuthentication("Bearer")
                 .AddJwtBearer("Bearer", options =>
                 {
@@ -23,6 +24,16 @@ namespace Api
                         ValidateAudience = false
                     };
                 });
+            
+            // adds an authorization policy to make sure the token is for scope 'api1'
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("ApiScope", policy =>
+                {
+                    policy.RequireAuthenticatedUser();
+                    policy.RequireClaim("scope", "api1");
+                });
+            });
         }
 
         public void Configure(IApplicationBuilder app)
@@ -34,7 +45,8 @@ namespace Api
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllers();
+                endpoints.MapControllers()
+                    .RequireAuthorization("ApiScope");
             });
         }
     }
