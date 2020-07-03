147387ae38ed9e65ba2637fcddfa81f6491d554b
Brock Allen <brockallen@gmail.com>
2020-07-03 10:24:36 -0400

    fix missing check for redirect

--- src/IdentityServer4/src/ResponseHandling/Default/AuthorizeInteractionResponseGenerator.cs
+++ src/IdentityServer4/src/ResponseHandling/Default/AuthorizeInteractionResponseGenerator.cs
@@ -93,17 +93,17 @@ namespace IdentityServer4.ResponseHandling
 
             var result = await ProcessLoginAsync(request);
 
-            if (result.IsLogin && request.PromptModes.Contains(OidcConstants.PromptModes.None))
+            if ((result.IsLogin || result.IsRedirect) && request.PromptModes.Contains(OidcConstants.PromptModes.None))
             {
                 // prompt=none means do not show the UI
                 Logger.LogInformation("Changing response to LoginRequired: prompt=none was requested");
                 result = new InteractionResponse
                 {
-                    Error = OidcConstants.AuthorizeErrors.LoginRequired
+                    Error = result.IsLogin ? OidcConstants.AuthorizeErrors.LoginRequired : OidcConstants.AuthorizeErrors.InteractionRequired
                 };
             }
 
-            if (result.IsLogin || result.IsError)
+            if (result.IsLogin || result.IsError || result.IsRedirect)
             {
                 return result;
             }
