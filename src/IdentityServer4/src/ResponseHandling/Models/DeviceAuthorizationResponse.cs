1929707b8b1634b42071ab3f649c4f2cbc71e954
Michael Brunner <michael.brunner@xitaso.com>
2020-11-06 19:59:22 +0100

    #5022 Adding more data to the DeviceAuthorizationSuccessEvent to allow different notification mechanism for pending new devices

--- src/IdentityServer4/src/ResponseHandling/Models/DeviceAuthorizationResponse.cs
+++ src/IdentityServer4/src/ResponseHandling/Models/DeviceAuthorizationResponse.cs
@@ -6,14 +6,39 @@
 
 namespace IdentityServer4.ResponseHandling
 {
+    /// <summary>
+    /// Response to request of device flow rfc8628
+    /// </summary>
     public class DeviceAuthorizationResponse
     {
+        /// <summary>
+        /// Code not shown to the user but used to attempt to retrieve tokens via token endpoint with grant type urn:ietf:params:oauth:grant-type:device_code
+        /// </summary>
         public string DeviceCode { get; set; }
+        
+        /// <summary>
+        /// The code which the user has to enter to allow this device.
+        /// </summary>
         public string UserCode { get; set; }
+        
+        /// <summary>
+        /// VerificationUri to be opened in browser without UserCode
+        /// </summary>
         public string VerificationUri { get; set; }
 
+        /// <summary>
+        /// VerificationUri to be opened in browser including the UserCode
+        /// </summary>
         public string VerificationUriComplete { get; set; }
+        
+        /// <summary>
+        /// Lifetime in seconds until this device flow request expires.
+        /// </summary>
         public int DeviceCodeLifetime { get; set; }
+        
+        /// <summary>
+        /// The minimum amount of time in seconds needs to wait between polling requests to the token endpoint.
+        /// </summary>
         public int Interval { get; set; }
     }
 }
